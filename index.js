990082a855b4272e4906f4ca0eeb95e8789c8633
Juan Muñoz <juan.munoz@scality.com>
2016-04-07 15:28:54 +0200

    FT: Export /lib/https
     Arsenal now exports an additional object { ciphers, dhparam }.

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ module.exports = {
     errors: require('./lib/errors.js'),
     shuffle: require('./lib/shuffle'),
     stringHash: require('./lib/stringHash'),
+    https: {
+        ciphers: require('./lib/https/ciphers.js'),
+        dhparam: require('./lib/https/dh2048.js'),
+    },
 };
