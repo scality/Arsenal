ea4eb6a4fb3228b6efaee8063790393ed0aee70b
Antonin Coulibaly <antonin.coulibaly@scality.com>
2016-04-08 15:22:51 +0200

    Implement error map
    * Implement associated unit tests

--- tests/unit/errors.js
+++ tests/unit/errors.js
@@ -3,6 +3,17 @@ const assert = require('assert');
 const errorsJSON = require('../../errors/arsenalErrors.json');
 const errors = require('../../index').errors;
 
+const map = {
+    NoSuchBucket: 'NoSuchBucket',
+    BucketAlreadyExists: 'BucketAlreadyExists',
+    NoSuchKey: 'NoSuchKey',
+    DBNotFound: 'NoSuchBucket',
+    DBAlreadyExists: 'BucketAlreadyExists',
+    ObjNotFound: 'NoSuchKey',
+    NotImplemented: 'NotImplemented',
+};
+
+
 describe('Runtime errors instance generation', () => {
     Object.keys(errors).forEach(index => {
         it(`should return and instance of ${index} Error`, done => {
@@ -15,6 +26,16 @@ describe('Runtime errors instance generation', () => {
                                    'Incorrect description');
             assert.deepStrictEqual(errors[index][index], true,
                                    `${index} key must be true`);
+            done();
+        });
+    });
+});
+
+describe('Error map(wrapper) MD -> S3', () => {
+    Object.keys(map).forEach(index => {
+        it(`should return an instance of ${map[index]} Error`, done => {
+            assert.deepStrictEqual(errors[index].errorsMap(),
+                                   errors[map[index]]);
             done();
         });
     });
