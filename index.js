9adc02a6ba3b1e4ded1ae59b3c5586fb1f8f9755
Antonin Coulibaly <antonin.coulibaly@scality.com>
2016-04-25 12:27:02 +0200

    (errors) Implement vault error clean

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     db: require('./lib/db'),
-    errors: require('./lib/errors.js'),
+    errors: require('./lib/errors.js').errorsGen,
+    errorsClean: require('./lib/errors.js').errorsClean,
     shuffle: require('./lib/shuffle'),
     stringHash: require('./lib/stringHash'),
 };
