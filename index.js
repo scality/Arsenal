f41e5b1b3f7c91c4db65c1966fcca51e3ca5f6fb
Vianney Rancurel <vianney.rancurel@scality.com>
2016-06-03 01:27:02 +0200

    Move extensions to Arsenal

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ module.exports = {
         ciphers: require('./lib/https/ciphers.js'),
         dhparam: require('./lib/https/dh2048.js'),
     },
+    delimiter: require('./lib/extension/delimiter.extension'),
+    listMPU: require('./lib/extension/listMPU.extension'),
 };
