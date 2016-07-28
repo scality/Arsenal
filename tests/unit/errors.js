2b310a2f6a87a581b520720a2f904faa25c720d1
alexandre-merle <alexandre.merle@scality.com>
2016-07-27 17:28:31 -0700

    [Errors](ft): Allowing custom descriptions
    - Allow custom descriptions in arsenal errors

--- tests/unit/errors.js
+++ tests/unit/errors.js
@@ -3,19 +3,27 @@ const assert = require('assert');
 const errorsJSON = require('../../errors/arsenalErrors.json');
 const errors = require('../../index').errors;
 
-describe('Runtime errors instance generation', () => {
+describe('Errors: ', () => {
     Object.keys(errors).forEach(index => {
         it(`should return and instance of ${index} Error`, done => {
-            assert.deepStrictEqual(errors[index] instanceof Error, true,
-                                   'should be an instance of Error');
-            assert.deepStrictEqual(errors[index].code, errorsJSON[index].code,
-                                   'Wrong Code');
-            assert.deepStrictEqual(errors[index].description,
-                                   errorsJSON[index].description,
-                                   'Incorrect description');
-            assert.deepStrictEqual(errors[index][index], true,
-                                   `${index} key must be true`);
+            assert.strictEqual(errors[index] instanceof Error, true,
+                'should be an instance of Error');
+            assert.strictEqual(errors[index].code, errorsJSON[index].code,
+                'Wrong Code');
+            assert.strictEqual(errors[index].description,
+                errorsJSON[index].description, 'Incorrect description');
+            assert.strictEqual(errors[index][index], true,
+                `${index} key must be true`);
             done();
         });
+    });
+
+    it('should allow custom error descriptions', () => {
+        const originDescription = errors.NoSuchEntity.description;
+        const error =
+            errors.NoSuchEntity.customizeDescription('custom-description');
+        assert.strictEqual(errors.NoSuchEntity.description, originDescription);
+        assert.strictEqual(error.description, 'custom-description');
+        assert.strictEqual(error.NoSuchEntity, true);
     });
 });
