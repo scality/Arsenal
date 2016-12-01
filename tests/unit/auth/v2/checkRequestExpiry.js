52a5dd28b2dd53624260e8f0cbbb7724e6836439
Nicolas Humbert <nicolas.humbert@live.fr>
2016-12-01 11:32:41 -0800

    COMPAT: Date before epoch FIXES #198

--- tests/unit/auth/v2/checkRequestExpiry.js
+++ tests/unit/auth/v2/checkRequestExpiry.js
@@ -5,25 +5,36 @@ const assert = require('assert');
 const checkRequestExpiry =
     require('../../../../lib/auth/v2/checkRequestExpiry');
 const DummyRequestLogger = require('../../helpers').DummyRequestLogger;
+const errors = require('../../../../index').errors;
 
 const log = new DummyRequestLogger();
 
 describe('checkTimestamp for timecheck in header auth', () => {
-    it('should return true if the date in the header is ' +
-       'more than 15 minutes old', () => {
+    it('should return AccessDenied error if the date in the ' +
+       'header is before epochTime', () => {
+        const timestamp = new Date('1950-01-01');
+        const timeoutResult = checkRequestExpiry(timestamp, log);
+        assert.deepStrictEqual(timeoutResult, errors.AccessDenied);
+    });
+
+    it('should return RequestTimeTooSkewed error if the date in the ' +
+       'header is more than 15 minutes old', () => {
         const timestamp = new Date(Date.now() - 16 * 60000);
-        assert(checkRequestExpiry(timestamp, log));
+        const timeoutResult = checkRequestExpiry(timestamp, log);
+        assert.deepStrictEqual(timeoutResult, errors.RequestTimeTooSkewed);
     });
 
-    it('should return true if the date in the header is more ' +
-       'than 15 minutes in the future', () => {
+    it('should return RequestTimeTooSkewed error if the date in ' +
+       'the header is more than 15 minutes in the future', () => {
         const timestamp = new Date(Date.now() + 16 * 60000);
-        assert(checkRequestExpiry(timestamp, log));
+        const timeoutResult = checkRequestExpiry(timestamp, log);
+        assert.deepStrictEqual(timeoutResult, errors.RequestTimeTooSkewed);
     });
 
-    it('should return false if the date in the header is ' +
+    it('should return no error if the date in the header is ' +
        'within 15 minutes of current time', () => {
         const timestamp = new Date();
-        assert(!checkRequestExpiry(timestamp, log));
+        const timeoutResult = checkRequestExpiry(timestamp, log);
+        assert.deepStrictEqual(timeoutResult, undefined);
     });
 });
