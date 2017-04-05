ac145f19a7188312e6c15679076f3de30a7033ff
Rahul Padigela <rahul.padigela@scality.com>
2017-04-05 16:24:25 -0700

    cleanup: make version id tests independent

--- tests/unit/versioning/VersionID.js
+++ tests/unit/versioning/VersionID.js
@@ -12,32 +12,30 @@ function randkey(length) {
 describe('test generating versionIds', () => {
     const count = 1000;
     const vids = Array(count).fill(null);
+    for (let i = 0; i < count; i++) {
+        vids[i] = VID.generateVersionId(randkey(15));
+    }
     process.env.VID_CRYPTO_PASSWORD = randkey(64);
 
-    it('sorted in reversed chronological and alphabetical order', done => {
-        let previd = null;
+    it('sorted in reversed chronological and alphabetical order', () => {
         for (let i = 0; i < count; i++) {
-            vids[i] = VID.generateVersionId(randkey(15));
-            if (previd) {
-                assert(previd > vids[i], 'versionIds increased');
-                previd = vids[i];
+            if (i !== 0) {
+                assert(vids[i - 1] > vids[i],
+                    'previous VersionID is higher than its next');
             }
         }
-        done();
     });
 
-    it('should return error decoding non-hex string versionIds', done => {
+    it('should return error decoding non-hex string versionIds', () => {
         const encoded = vids.map(vid => VID.encode(vid));
         const decoded = encoded.map(vid => VID.decode(`${vid}foo`));
         decoded.forEach(result => assert(result instanceof Error));
-        done();
     });
 
-    it('should encode and decode versionIds', done => {
+    it('should encode and decode versionIds', () => {
         const encoded = vids.map(vid => VID.encode(vid));
         const decoded = encoded.map(vid => VID.decode(vid));
         assert.strictEqual(vids.length, count);
         assert.deepStrictEqual(vids, decoded);
-        done();
     });
 });
