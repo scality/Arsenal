99f1a3be4d3f60cf198653cb014aa4ad2b77bda7
Electra Chong <electra.chong@scality.com>
2017-07-25 17:03:18 -0700

    ft: extract MD5Sum util

---
+++ tests/utils/DummyRequest.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+class DummyRequest extends http.IncomingMessage {
+    constructor(obj, msg) {
+        super();
+        Object.keys(obj).forEach(x => {
+            this[x] = obj[x];
+        });
+        const contentLength = this.headers['content-length'];
+        if (this.parsedContentLength === undefined) {
+            if (contentLength !== undefined) {
+                this.parsedContentLength = parseInt(contentLength, 10);
+            } else if (msg !== undefined) {
+                this.parsedContentLength = msg.length;
+            } else {
+                this.parsedContentLength = 0;
+            }
+        }
+
+        if (Array.isArray(msg)) {
+            msg.forEach(part => {
+                this.push(part);
+            });
+        } else {
+            this.push(msg);
+        }
+        this.push(null);
+    }
+}
+
+module.exports = DummyRequest;
