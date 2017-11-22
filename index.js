56fdb5c511929fa919870cefe7d549d21c92f81b
Guillaume Gimenez <ploki@blackmilk.fr>
2017-11-22 11:58:48 -0800

    ft: S3C-1065: Null part generator
    This provides a new class, NullStream, that will be used to read null parts.
    (null parts are parts with null keys which are generated by the NFS server when
    growing a file with truncate)

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ module.exports = {
             require('./lib/s3middleware/validateConditionalHeaders')
             .validateConditionalHeaders,
         MD5Sum: require('./lib/s3middleware/MD5Sum'),
+        NullStream: require('./lib/s3middleware/nullStream'),
         objectUtils: require('./lib/s3middleware/objectUtils'),
         azureHelper: {
             mpuUtils:
