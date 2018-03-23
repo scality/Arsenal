scality/Arsenal

commit ea8d523501fcd996447986318e59a95e729563b0

ZENKO-140 rf: extract metadata backends from S3

lib/storage/metadata/mongoclient/MongoClientInterface.js
@@ -11,6 +11,7 @@
  */
 const async = require('async');
 
+const format = require('util').format;
 const constants = require('../../../constants');
 
 const errors = require('../../../errors');
@@ -33,8 +34,6 @@ const __UUID = 'uuid';
 const ASYNC_REPAIR_TIMEOUT = 15000;
 
 let uidCounter = 0;
-
-const format = require('util').format;
 
 const VID_SEP = require('../../../versioning/constants')
     .VersioningConstants.VersionId.Separator;
@@ -70,55 +69,51 @@ function generatePHDVersion(versionId) {
  * @param {Object} params - constructor params
  * @param {String} params.host - host for mongo
  * @param {String} params.port - port for mongo
- * @param {String} params.writeConcern - writeConcern for mongo
- * @param {String} params.replicaSet - replicaSet for mongo
- * @param {String} params.readPreference - readPreference for mongo
+ * @param {String} params.replicationGroupId - replication group id
+ * used here to generate version id's
+ * @param {String} params.replicaSet - name of mongo replica setup
+ * @param {String} params.path - path value
+ * // Does backbeat use this at all? Can we make this optional and
+ * // set a default so when instantiate the client elsewhere don't need?
  * @param {String} params.database - name of database
  * @param {werelogs.Logger} params.logger - logger instance
  * @param {String} [params.path] - path for mongo volume
- * @param {String} [params.replicationGroupId] - replication group id
- * used here to generate version id's on puts. S3 instantiates the class
- * but backbeat does not
  */
 class MongoClientInterface {
-
     constructor(params) {
-        const { hosts,
-                writeConcern, replicaSet, readPreference,
-                path, database, logger,
-		replicationGroupId } = params;
-        const mongoUrl = format(
-            'mongodb://%s/?w=%s&replicaSet=%s&readPreference=%s',
-            hosts,
-            writeConcern,
-            replicaSet,
-            readPreference);
+        const { host, path, database, logger,
+            replicationGroupId, replicaSet } = params;
+        this.mongoUrl = format(
+            'mongodb://%s/?replicaSet=%s',
+            host,
+            replicaSet);
         this.logger = logger;
         this.client = null;
         this.db = null;
         this.path = path;
         this.replicationGroupId = replicationGroupId;
         this.database = database;
-        this.logger.info(`connecting to ${mongoUrl}`);
+    }
+
+    setup(cb) {
         // FIXME: constructors shall not have side effect so there
         // should be an async_init(cb) method in the wrapper to
         // initialize this backend
-        MongoClient.connect(mongoUrl, (err, client) => {
+        return MongoClient.connect(this.mongoUrl, (err, client) => {
             if (err) {
-                this.logger.error('error connecting to mongo server',
-                { error: err.message });
+                this.logger.error('error connecting to mongodb',
+                    { error: err.message });
                 throw (errors.InternalError);
             }
-            this.logger.debug('connected to mongodb');
+            this.logger.info('connected to mongodb');
             this.client = client;
             this.db = client.db(this.database, {
                 ignoreUndefined: true,
             });
-            this.usersBucketHack();
+            return this.usersBucketHack(cb);
         });
     }
-
-    usersBucketHack() {
+    usersBucketHack(cb) {
         /* FIXME: Since the bucket creation API is expecting the
            usersBucket to have attributes, we pre-create the
            usersBucket attributes here (see bucketCreation.js line
@@ -126,7 +121,7 @@ class MongoClientInterface {
         const usersBucketAttr = new BucketInfo(constants.usersBucket,
             'admin', 'admin', new Date().toJSON(),
             BucketInfo.currentModelVersion());
-        this.createBucket(
+        return this.createBucket(
             constants.usersBucket,
             usersBucketAttr, {}, err => {
                 if (err) {
@@ -135,6 +130,7 @@ class MongoClientInterface {
                                       { error: err });
                     throw (errors.InternalError);
                 }
+                return cb();
             });
     }
 
@@ -180,7 +176,7 @@ class MongoClientInterface {
             if (err) {
                 log.error(
                     'getBucketAttributes: error getting bucket attributes',
-                { error: err.message });
+                    { error: err.message });
                 return cb(errors.InternalError);
             }
             if (!doc) {
@@ -300,8 +296,6 @@ class MongoClientInterface {
      * sequentially create the object THEN update the master
      */
     putObjectVerCase1(c, bucketName, objName, objVal, params, log, cb) {
-        // TODO: if backbeat is doing a put, versionId should be pulled
-        // from existing metadata
         const versionId = generateVersionId(this.replicationGroupId);
         // eslint-disable-next-line
         objVal.versionId = versionId;
@@ -344,8 +338,6 @@ class MongoClientInterface {
      * have been created with versions
      */
     putObjectVerCase2(c, bucketName, objName, objVal, params, log, cb) {
-        // TODO: if backbeat is doing a put, versionId should be pulled
-        // from existing metadata
         const versionId = generateVersionId(this.replicationGroupId);
         // eslint-disable-next-line
         objVal.versionId = versionId;
@@ -621,8 +613,6 @@ class MongoClientInterface {
      */
     deleteObjectVerMaster(c, bucketName, objName, params, log, cb) {
         const vObjName = formatVersionKey(objName, params.versionId);
-        // TODO: if backbeat is doing a delete, versionId should be pulled
-        // from existing metadata
         const _vid = generateVersionId(this.replicationGroupId);
         const mst = generatePHDVersion(_vid);
         c.bulkWrite([{
@@ -746,8 +736,7 @@ class MongoClientInterface {
         const requestParams = extension.genMDParams();
         const c = this.getCollection(bucketName);
         let cbDone = false;
-        const stream = new MongoReadStream(c, requestParams,
-                                           params.mongifiedSearch);
+        const stream = new MongoReadStream(c, requestParams);
         stream
             .on('data', e => {
                 if (extension.filter(e) < 0) {

index.js
@@ -72,14 +72,34 @@ module.exports = {
     },
     storage: {
         metadata: {
-            MetadataFileServer:
-            require('./lib/storage/metadata/file/MetadataFileServer'),
-            MetadataFileClient:
-            require('./lib/storage/metadata/file/MetadataFileClient'),
-            LogConsumer:
-            require('./lib/storage/metadata/bucketclient/LogConsumer'),
-            MongoClientInterface:
-            require('./lib/storage/metadata/mongoclient/MongoClientInterface'),
+            MetadataWrapper: require('./lib/storage/metadata/MetadataWrapper'),
+            bucketclient: {
+                BucketClientInterface:
+                require('./lib/storage/metadata/bucketclient/' +
+                    'BucketClientInterface'),
+                LogConsumer:
+                require('./lib/storage/metadata/bucketclient/LogConsumer'),
+            },
+            file: {
+                BucketFileInterface:
+                require('./lib/storage/metadata/file/BucketFileInterface'),
+                MetadataFileServer:
+                require('./lib/storage/metadata/file/MetadataFileServer'),
+                MetadataFileClient:
+                require('./lib/storage/metadata/file/MetadataFileClient'),
+            },
+            inMemory: {
+                metastore:
+                require('./lib/storage/metadata/in_memory/metastore'),
+                metadata: require('./lib/storage/metadata/in_memory/metadata'),
+                bucketUtilities:
+                require('./lib/storage/metadata/in_memory/bucket_utilities'),
+            },
+            mongoclient: {
+                MongoClientInterface:
+                require('./lib/storage/metadata/mongoclient/' +
+                    'MongoClientInterface'),
+            },
         },
         data: {
             file: {

lib/storage/metadata/MetadataWrapper.js
@@ -1,0 +1,340 @@
+const errors = require('../../errors');
+
+const BucketInfo = require('../../models/BucketInfo');
+
+const BucketClientInterface = require('./bucketclient/BucketClientInterface');
+const BucketFileInterface = require('./file/BucketFileInterface');
+const MongoClientInterface = require('./mongoclient/MongoClientInterface');
+const metastore = require('./in_memory/metastore');
+
+let CdmiMetadata;
+try {
+    CdmiMetadata = require('cdmiclient').CdmiMetadata;
+} catch (err) {
+    CdmiMetadata = null;
+}
+
+let bucketNotificationHook;
+
+/** _parseListEntries - parse the values returned in a listing by metadata
+ * @param {object[]} entries - Version or Content entries in a metadata listing
+ * @param {string} entries[].key - metadata key
+ * @param {string} entries[].value - stringified object metadata
+ * @return {object} - mapped array with parsed value or JSON parsing err
+ */
+function _parseListEntries(entries) {
+    return entries.map(entry => {
+        const tmp = JSON.parse(entry.value);
+        return {
+            key: entry.key,
+            value: {
+                Size: tmp['content-length'],
+                ETag: tmp['content-md5'],
+                VersionId: tmp.versionId,
+                IsNull: tmp.isNull,
+                IsDeleteMarker: tmp.isDeleteMarker,
+                LastModified: tmp['last-modified'],
+                Owner: {
+                    DisplayName: tmp['owner-display-name'],
+                    ID: tmp['owner-id'],
+                },
+                StorageClass: tmp['x-amz-storage-class'],
+                // MPU listing properties
+                Initiated: tmp.initiated,
+                Initiator: tmp.initiator,
+                EventualStorageBucket: tmp.eventualStorageBucket,
+                partLocations: tmp.partLocations,
+                creationDate: tmp.creationDate,
+            },
+        };
+    });
+}
+
+/** parseListEntries - parse the values returned in a listing by metadata
+ * @param {object[]} entries - Version or Content entries in a metadata listing
+ * @param {string} entries[].key - metadata key
+ * @param {string} entries[].value - stringified object metadata
+ * @return {(object|Error)} - mapped array with parsed value or JSON parsing err
+ */
+function parseListEntries(entries) {
+    // wrap private function in a try/catch clause
+    // just in case JSON parsing throws an exception
+    try {
+        return _parseListEntries(entries);
+    } catch (e) {
+        return e;
+    }
+}
+
+class MetadataWrapper {
+    constructor(clientName, params, logger) {
+        if (clientName === 'mem') {
+            this.client = metastore;
+            this.implName = 'memorybucket';
+        } else if (clientName === 'file') {
+            this.client = new BucketFileInterface(params, logger);
+            this.implName = 'bucketfile';
+        } else if (clientName === 'scality') {
+            this.client = new BucketClientInterface(params, logger);
+            this.implName = 'bucketclient';
+        } else if (clientName === 'mongodb') {
+            this.client = new MongoClientInterface({
+                host: params.mongodb.host,
+                port: params.mongodb.port,
+                writeConcern: params.mongodb.writeConcern,
+                replicaSet: params.mongodb.replicaSet,
+                readPreference: params.mongodb.readPreference,
+                database: params.mongodb.database,
+                replicationGroupId: params.replicationGroupId,
+                path: params.mongodb.path,
+                logger,
+            });
+            this.implName = 'mongoclient';
+        } else if (clientName === 'cdmi') {
+            if (!CdmiMetadata) {
+                throw new Error('Unauthorized backend');
+            }
+
+            this.client = new CdmiMetadata({
+                path: params.cdmi.path,
+                host: params.cdmi.host,
+                port: params.cdmi.port,
+                readonly: params.cdmi.readonly,
+            });
+            this.implName = 'cdmi';
+        }
+    }
+
+    setup(done) {
+        if (this.client.setup) {
+            return this.client.setup(done);
+        }
+        return process.nextTick(() => done);
+    }
+
+    createBucket(bucketName, bucketMD, log, cb) {
+        log.debug('creating bucket in metadata');
+        this.client.createBucket(bucketName, bucketMD, log, err => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    error: err });
+                return cb(err);
+            }
+            log.trace('bucket created in metadata');
+            if (bucketNotificationHook) {
+                setTimeout(bucketNotificationHook);
+            }
+            return cb(err);
+        });
+    }
+
+    updateBucket(bucketName, bucketMD, log, cb) {
+        log.debug('updating bucket in metadata');
+        this.client.putBucketAttributes(bucketName, bucketMD, log, err => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    error: err });
+                return cb(err);
+            }
+            log.trace('bucket updated in metadata');
+            return cb(err);
+        });
+    }
+
+    getBucket(bucketName, log, cb) {
+        log.debug('getting bucket from metadata');
+        this.client.getBucketAttributes(bucketName, log, (err, data) => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    error: err });
+                return cb(err);
+            }
+            log.trace('bucket retrieved from metadata');
+            return cb(err, BucketInfo.fromObj(data));
+        });
+    }
+
+    deleteBucket(bucketName, log, cb) {
+        log.debug('deleting bucket from metadata');
+        this.client.deleteBucket(bucketName, log, err => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    error: err });
+                return cb(err);
+            }
+            log.debug('Deleted bucket from Metadata');
+            if (bucketNotificationHook) {
+                setTimeout(bucketNotificationHook);
+            }
+            return cb(err);
+        });
+    }
+
+    putObjectMD(bucketName, objName, objVal, params, log, cb) {
+        log.debug('putting object in metadata');
+        const value = typeof objVal.getValue === 'function' ?
+            objVal.getValue() : objVal;
+        this.client.putObject(bucketName, objName, value, params, log,
+        (err, data) => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    error: err });
+                return cb(err);
+            }
+            if (data) {
+                log.debug('object version successfully put in metadata',
+                { version: data });
+            } else {
+                log.debug('object successfully put in metadata');
+            }
+            return cb(err, data);
+        });
+    }
+
+    getBucketAndObjectMD(bucketName, objName, params, log, cb) {
+        log.debug('getting bucket and object from metadata',
+                  { database: bucketName, object: objName });
+        this.client.getBucketAndObject(bucketName, objName, params, log,
+            (err, data) => {
+                if (err) {
+                    log.debug('error from metadata', { implName: this.implName,
+                        err });
+                    return cb(err);
+                }
+                log.debug('bucket and object retrieved from metadata',
+                { database: bucketName, object: objName });
+                return cb(err, data);
+            });
+    }
+
+    getObjectMD(bucketName, objName, params, log, cb) {
+        log.debug('getting object from metadata');
+        this.client.getObject(bucketName, objName, params, log, (err, data) => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    err });
+                return cb(err);
+            }
+            log.debug('object retrieved from metadata');
+            return cb(err, data);
+        });
+    }
+
+    deleteObjectMD(bucketName, objName, params, log, cb) {
+        log.debug('deleting object from metadata');
+        this.client.deleteObject(bucketName, objName, params, log, err => {
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    err });
+                return cb(err);
+            }
+            log.debug('object deleted from metadata');
+            return cb(err);
+        });
+    }
+
+    listObject(bucketName, listingParams, log, cb) {
+        if (listingParams.listingType === undefined) {
+            // eslint-disable-next-line
+            listingParams.listingType = 'Delimiter';
+        }
+        this.client.listObject(bucketName, listingParams, log, (err, data) => {
+            log.debug('getting object listing from metadata');
+            if (err) {
+                log.debug('error from metadata', { implName: this.implName,
+                    err });
+                return cb(err);
+            }
+            log.debug('object listing retrieved from metadata');
+            if (listingParams.listingType === 'DelimiterVersions') {
+                // eslint-disable-next-line
+                data.Versions = parseListEntries(data.Versions);
+                if (data.Versions instanceof Error) {
+                    log.error('error parsing metadata listing', {
+                        error: data.Versions,
+                        listingType: listingParams.listingType,
+                        method: 'listObject',
+                    });
+                    return cb(errors.InternalError);
+                }
+                return cb(null, data);
+            }
+            // eslint-disable-next-line
+            data.Contents = parseListEntries(data.Contents);
+            if (data.Contents instanceof Error) {
+                log.error('error parsing metadata listing', {
+                    error: data.Contents,
+                    listingType: listingParams.listingType,
+                    method: 'listObject',
+                });
+                return cb(errors.InternalError);
+            }
+            return cb(null, data);
+        });
+    }
+
+    listMultipartUploads(bucketName, listingParams, log, cb) {
+        this.client.listMultipartUploads(bucketName, listingParams, log,
+            (err, data) => {
+                log.debug('getting mpu listing from metadata');
+                if (err) {
+                    log.debug('error from metadata', { implName: this.implName,
+                        err });
+                    return cb(err);
+                }
+                log.debug('mpu listing retrieved from metadata');
+                return cb(err, data);
+            });
+    }
+
+    switch(newClient, cb) {
+        this.client = newClient;
+        return cb();
+    }
+
+    checkHealth(log, cb) {
+        if (!this.client.checkHealth) {
+            const defResp = {};
+            defResp[this.implName] = { code: 200, message: 'OK' };
+            return cb(null, defResp);
+        }
+        return this.client.checkHealth(this.implName, log, cb);
+    }
+
+    getUUID(log, cb) {
+        if (!this.client.getUUID) {
+            log.debug('returning empty uuid as fallback', {
+                implName: this.implName });
+            return cb(null, '');
+        }
+        return this.client.getUUID(log, cb);
+    }
+
+    getDiskUsage(log, cb) {
+        if (!this.client.getDiskUsage) {
+            log.debug('returning empty disk usage as fallback', {
+                implName: this.implName });
+            return cb(null, {});
+        }
+        return this.client.getDiskUsage(cb);
+    }
+
+    countItems(log, cb) {
+        if (!this.client.countItems) {
+            log.debug('returning zero item counts as fallback', {
+                implName: this.implName });
+            return cb(null, {
+                buckets: 0,
+                objects: 0,
+                versions: 0,
+            });
+        }
+        return this.client.countItems(log, cb);
+    }
+
+    notifyBucketChange(cb) {
+        bucketNotificationHook = cb;
+    }
+}
+
+module.exports = MetadataWrapper;

lib/storage/metadata/bucketclient/BucketClientInterface.js
@@ -1,0 +1,137 @@
+const assert = require('assert');
+
+const BucketInfo = require('../../../models/BucketInfo');
+
+class BucketClientInterface {
+    constructor(params, bucketclient, logger) {
+        assert(params.bucketdBootstrap.length > 0,
+               'bucketd bootstrap list is empty');
+        const bootstrap = params.bucketdBootstrap;
+        const log = params.bucketdLog;
+        if (params.https) {
+            const { key, cert, ca } = params.https;
+            logger.info('bucketclient configuration', {
+                bootstrap,
+                log,
+                https: true,
+            });
+            this.client = new bucketclient.RESTClient(bootstrap, log, true,
+                key, cert, ca);
+        } else {
+            logger.info('bucketclient configuration', {
+                bootstrap,
+                log,
+                https: false,
+            });
+            this.client = new bucketclient.RESTClient(bootstrap, log);
+        }
+    }
+
+    createBucket(bucketName, bucketMD, log, cb) {
+        this.client.createBucket(bucketName, log.getSerializedUids(),
+                                 bucketMD.serialize(), cb);
+        return null;
+    }
+
+    getBucketAttributes(bucketName, log, cb) {
+        this.client.getBucketAttributes(bucketName, log.getSerializedUids(),
+            (err, data) => {
+                if (err) {
+                    return cb(err);
+                }
+                return cb(err, BucketInfo.deSerialize(data));
+            });
+        return null;
+    }
+
+    getBucketAndObject(bucketName, objName, params, log, cb) {
+        this.client.getBucketAndObject(bucketName, objName,
+            log.getSerializedUids(), (err, data) => {
+                if (err && (!err.NoSuchKey && !err.ObjNotFound)) {
+                    return cb(err);
+                }
+                return cb(null, JSON.parse(data));
+            }, params);
+        return null;
+    }
+
+    putBucketAttributes(bucketName, bucketMD, log, cb) {
+        this.client.putBucketAttributes(bucketName, log.getSerializedUids(),
+                                        bucketMD.serialize(), cb);
+        return null;
+    }
+
+    deleteBucket(bucketName, log, cb) {
+        this.client.deleteBucket(bucketName, log.getSerializedUids(), cb);
+        return null;
+    }
+
+    putObject(bucketName, objName, objVal, params, log, cb) {
+        this.client.putObject(bucketName, objName, JSON.stringify(objVal),
+            log.getSerializedUids(), cb, params);
+        return null;
+    }
+
+    getObject(bucketName, objName, params, log, cb) {
+        this.client.getObject(bucketName, objName, log.getSerializedUids(),
+            (err, data) => {
+                if (err) {
+                    return cb(err);
+                }
+                return cb(err, JSON.parse(data));
+            }, params);
+        return null;
+    }
+
+    deleteObject(bucketName, objName, params, log, cb) {
+        this.client.deleteObject(bucketName, objName, log.getSerializedUids(),
+                                 cb, params);
+        return null;
+    }
+
+    listObject(bucketName, params, log, cb) {
+        this.client.listObject(bucketName, log.getSerializedUids(), params,
+            (err, data) => {
+                if (err) {
+                    return cb(err);
+                }
+                return cb(err, JSON.parse(data));
+            });
+        return null;
+    }
+
+    listMultipartUploads(bucketName, params, log, cb) {
+        this.client.listObject(bucketName, log.getSerializedUids(), params,
+            (err, data) => {
+                if (err) {
+                    return cb(err);
+                }
+                return cb(null, JSON.parse(data));
+            });
+        return null;
+    }
+
+    checkHealth(implName, log, cb) {
+        return this.client.healthcheck(log, (err, result) => {
+            const respBody = {};
+            if (err) {
+                log.error(`error from ${implName}`, { error: err });
+                respBody[implName] = {
+                    error: err,
+                };
+                // error returned as null so async parallel doesn't return
+                // before all backends are checked
+                return cb(null, respBody);
+            }
+            const parseResult = JSON.parse(result);
+            respBody[implName] = {
+                code: 200,
+                message: 'OK',
+                body: parseResult,
+            };
+            return cb(null, respBody);
+        });
+    }
+}
+
+module.exports = BucketClientInterface;

commit 9bf1c31f90ab7b4b7396f50fad14a934616877a0

fixing metadata search broken by commit ea8d523501fcd996447986318e59a95e729563b0

lib/storage/metadata/mongoclient/MongoClientInterface.js
@@ -732,7 +732,8 @@ class MongoClientInterface {
         const requestParams = extension.genMDParams();
         const c = this.getCollection(bucketName);
         let cbDone = false;
-        const stream = new MongoReadStream(c, requestParams);
+        const stream = new MongoReadStream(c, requestParams,
+                                           params.mongifiedSearch);
         stream
             .on('data', e => {
                 if (extension.filter(e) < 0) {