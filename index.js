f9bb82ce43839fc610318f0663006434dd0f78e6
JianqinWang <jianqin.wang@scality.com>
2019-01-30 12:58:22 -0800

    ZENKO-1446: metadata-mock for bb

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ module.exports = {
     Clustering: require('./lib/Clustering'),
     testing: {
         matrix: require('./lib/testing/matrix.js'),
+        MetadataMock: require('./lib/testing/MetadataMock.js'),
     },
     versioning: {
         VersioningConstants: require('./lib/versioning/constants.js')
