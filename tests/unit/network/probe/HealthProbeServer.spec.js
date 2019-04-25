00cfbafa554b6e998f181b77969cbe08a1217a51
Jonathan Gramain <jonathan.gramain@scality.com>
2019-04-24 18:46:39 -0700

    feature: ZENKO-1520 expose prometheus metrics in HealthProbeServer
    Add a new route '/_/monitoring/metrics' exposed by the
    HealthProbeServer that runs on pods, to expose default metrics (nodejs
    etc) and custom metrics in prometheus format. Then, prometheus will be
    able to scrape them for each pod.
    
    Ideally the class should be renamed, maybe to MonitoringServer, kept
    it for later as it involves a larger refactor.

--- tests/unit/network/probe/HealthProbeServer.spec.js
+++ tests/unit/network/probe/HealthProbeServer.spec.js
@@ -15,12 +15,12 @@ function makeRequest(meth, uri) {
     return req;
 }
 
-const endpoints = [
+const healthcheckEndpoints = [
     '/_/health/liveness',
     '/_/health/readiness',
 ];
 
-const badEndpoints = [
+const badHealthcheckEndpoints = [
     '/_/health/liveness_thisiswrong',
     '/_/health/readiness_thisiswrong',
 ];
@@ -42,7 +42,7 @@ describe('network.probe.HealthProbeServer', () => {
             server.stop();
             done();
         });
-        endpoints.forEach(ep => {
+        healthcheckEndpoints.forEach(ep => {
             it('should perform a GET and ' +
                 'return 200 OK', done => {
                 makeRequest('GET', ep)
@@ -82,7 +82,7 @@ describe('network.probe.HealthProbeServer', () => {
             done();
         });
 
-        endpoints.forEach(ep => {
+        healthcheckEndpoints.forEach(ep => {
             it('should perform a GET and ' +
                 'return 503 ServiceUnavailable', done => {
                 makeRequest('GET', ep)
@@ -117,7 +117,7 @@ describe('network.probe.HealthProbeServer', () => {
             done();
         });
 
-        endpoints.forEach(ep => {
+        healthcheckEndpoints.forEach(ep => {
             it('should perform a POST and ' +
                 'return 405 MethodNotAllowed', done => {
                 makeRequest('POST', ep)
@@ -152,7 +152,7 @@ describe('network.probe.HealthProbeServer', () => {
             done();
         });
 
-        badEndpoints.forEach(ep => {
+        badHealthcheckEndpoints.forEach(ep => {
             it('should perform a GET and ' +
                 'return 400 InvalidURI', done => {
                 makeRequest('GET', ep)
@@ -165,6 +165,87 @@ describe('network.probe.HealthProbeServer', () => {
                         done();
                     }).end();
             });
+        });
+    });
+
+    describe('metrics route', () => {
+        let server;
+        function setup(done) {
+            server = new HealthProbeServer({ port: 4042 });
+            server._cbOnListening = done;
+            server.start();
+        }
+
+        before(done => {
+            setup(done);
+        });
+
+        after(done => {
+            server.stop();
+            done();
+        });
+        it('should expose metrics in prometheus format', done => {
+            const counter = server.createCounter(
+                'gizmo_counter', 'Count gizmos');
+            counter.inc();
+            counter.inc(10);
+            const gauge = server.createGauge(
+                'gizmo_gauge', 'Measure gizmos');
+            gauge.set(42);
+            gauge.inc();
+            gauge.dec(10);
+
+            const savedCounter = server.getMetric('gizmo_counter');
+            // check we get the same original counter object
+            assert.strictEqual(savedCounter, counter);
+
+            const savedGauge = server.getMetric('gizmo_gauge');
+            // check we get the same original gauge object
+            assert.strictEqual(savedGauge, gauge);
+
+            assert.strictEqual(server.getMetric('does_not_exist'), undefined);
+
+            const expectedLines = [
+                '# HELP gizmo_counter Count gizmos',
+                '# TYPE gizmo_counter counter',
+                'gizmo_counter 11',
+                '# HELP gizmo_gauge Measure gizmos',
+                '# TYPE gizmo_gauge gauge',
+                'gizmo_gauge 33',
+            ];
+
+            makeRequest('GET', '/_/monitoring/metrics')
+                .on('response', res => {
+                    assert(res.statusCode === 200);
+                    const respBufs = [];
+                    res.on('data', data => {
+                        respBufs.push(data);
+                    });
+                    res.on('end', () => {
+                        const respContents = respBufs.join('');
+                        // check that each expected line is present in
+                        // the response
+                        const respLines = {};
+                        respContents.split('\n').forEach(line => {
+                            respLines[line.trimRight()] = true;
+                        });
+                        expectedLines.forEach(expectedLine => {
+                            assert.notStrictEqual(
+                                respLines[expectedLine], undefined,
+                                'missing expected line in response ' +
+                                    `'${expectedLine}'`);
+                        });
+                        done();
+                    });
+                    res.on('error', err => {
+                        assert.ifError(err);
+                        done();
+                    });
+                })
+                .on('error', err => {
+                    assert.ifError(err);
+                    done();
+                }).end();
         });
     });
 });
