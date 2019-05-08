7416c12702d913dbdf9e11b2a857217b1308019b
Rahul Padigela <rahulreddy@users.noreply.github.com>
2019-05-08 15:37:23 -0700

    use jest, jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  setupFiles: [],
+  setupFilesAfterEnv: ['<rootDir>/jest.postsetup.js'],
+  testMatch: ['**/tests/**/*.js?(x)']
+};
