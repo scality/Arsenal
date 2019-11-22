ad58f6698162b11ddf44452e4719bf11f26ade2e
Alexander Chan <alexander.chan@scality.com>
2019-11-21 17:26:49 -0800

    feature: ZENKO-2089 add mongodb conditionals
    add conditional put/delete operations to allow for correct blobserver
    behaviors

---
+++ tests/functional/metadata/mongodb/mongoClientInterface.js
@@ -0,0 +1,316 @@
+const assert = require('assert');
+const async = require('async');
+const werelogs = require('werelogs');
+const { MongoMemoryReplSet } = require('mongodb-memory-server');
+
+const errors = require('../../../../lib/errors');
+const logger = new werelogs.Logger('MongoClientInterface', 'debug', 'debug');
+const BucketInfo = require('../../../../lib/models/BucketInfo');
+const MetadataWrapper =
+    require('../../../../lib/storage/metadata/MetadataWrapper');
+
+const implName = 'mongodb';
+const dbName = 'metadata';
+const bucketName = 'testbucket';
+
+const mongoserver = new MongoMemoryReplSet({
+    debug: false,
+    instanceOpts: [
+        { port: 27018 },
+    ],
+    replSet: {
+        name: 'rs0',
+        count: 1,
+        dbName,
+        storageEngine: 'ephemeralForTest',
+    },
+});
+
+describe('MongoClientInterface', () => {
+    let metadata;
+    before(done => {
+        mongoserver.waitUntilRunning().then(() => {
+            const opts = {
+                mongodb: {
+                    replicaSetHosts: 'localhost:27018',
+                    writeConcern: 'majority',
+                    replicaSet: 'rs0',
+                    readPreference: 'primary',
+                    database: dbName,
+                },
+            };
+            metadata = new MetadataWrapper(implName, opts, null, logger);
+            metadata.setup(done);
+        });
+    });
+
+    after(done => {
+        async.series([
+            next => metadata.close(next),
+            next => mongoserver.stop()
+                .then(() => next())
+                .catch(next),
+        ], done);
+    });
+
+    beforeEach(done => {
+        const bucketMD = BucketInfo.fromObj({
+            _name: bucketName,
+            _owner: 'testowner',
+            _ownerDisplayName: 'testdisplayname',
+            _creationDate: new Date().toJSON(),
+            _acl: {
+                Canned: 'private',
+                FULL_CONTROL: [],
+                WRITE: [],
+                WRITE_ACP: [],
+                READ: [],
+                READ_ACP: [],
+            },
+            _mdBucketModelVersion: 10,
+            _transient: false,
+            _deleted: false,
+            _serverSideEncryption: null,
+            _versioningConfiguration: null,
+            _locationConstraint: 'us-east-1',
+            _readLocationConstraint: null,
+            _cors: null,
+            _replicationConfiguration: null,
+            _lifecycleConfiguration: null,
+            _uid: '',
+            _isNFS: null,
+            ingestion: null,
+        });
+        metadata.createBucket(bucketName, bucketMD, logger, done);
+    });
+
+    afterEach(done => {
+        metadata.deleteBucket(bucketName, logger, done);
+    });
+
+    describe('::putObjectWithCond', () => {
+        const tests = [
+            [
+                'should upsert object if an existing object does not exist',
+                {
+                    initVal: null,
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: { value: { number: 24 } },
+                    expectedVal: { value: { number: 42, string: 'forty-two' } },
+                    error: null,
+                },
+            ],
+            [
+                'should not update an existing object if the conditions fails',
+                {
+                    initVal: { value: { number: 0, string: 'zero' } },
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: { value: { number: 24 } },
+                    expectedVal: { value: { number: 0, string: 'zero' } },
+                    error: errors.InternalError,
+                },
+            ],
+            [
+                'should not update an existing object if the conditions fails',
+                {
+                    initVal: { value: { number: 0, string: 'zero' } },
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: { value: { string: { $eq: 'twenty-four' } } },
+                    expectedVal: { value: { number: 0, string: 'zero' } },
+                    error: errors.InternalError,
+                },
+            ],
+            [
+                'should not update an existing object if the conditions fails',
+                {
+                    initVal: { value: { number: 0, string: 'zero' } },
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: {
+                        value: {
+                            string: { $eq: 'twenty-four' },
+                            number: { $eq: 0 },
+                        },
+                    },
+                    expectedVal: { value: { number: 0, string: 'zero' } },
+                    error: errors.InternalError,
+                },
+            ],
+            [
+                'should update an existing object if the conditions passes',
+                {
+                    initVal: { value: { number: 24, string: 'twenty-four' } },
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: { value: { number: 24 } },
+                    expectedVal: { value: { number: 42, string: 'forty-two' } },
+                    error: null,
+                },
+            ],
+            [
+                'should update an existing object if the conditions passes',
+                {
+                    initVal: { value: { number: 24, string: 'twenty-four' } },
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: { value: { string: { $eq: 'twenty-four' } } },
+                    expectedVal: { value: { number: 42, string: 'forty-two' } },
+                    error: null,
+                },
+            ],
+            [
+                'should update an existing object if the conditions passes',
+                {
+                    initVal: { value: { number: 24, string: 'twenty-four' } },
+                    upsertVal: { value: { number: 42, string: 'forty-two' } },
+                    conditions: {
+                        value: {
+                            string: { $eq: 'twenty-four' },
+                            number: { $eq: 24 },
+                        },
+                    },
+                    expectedVal: { value: { number: 42, string: 'forty-two' } },
+                    error: null,
+                },
+            ],
+        ];
+        tests.forEach(([msg, testCase]) => it(msg, done => {
+            const objectKey = 'testkey';
+            const {
+                initVal, upsertVal, conditions, expectedVal, error,
+            } = testCase;
+            const params = { conditions };
+            async.series([
+                next => {
+                    if (!initVal) {
+                        return next();
+                    }
+                    return metadata.putObjectMD(bucketName, objectKey, initVal,
+                                                {}, logger, next);
+                },
+                next => metadata.putObjectWithCond(bucketName, objectKey,
+                    upsertVal, params, logger, err => {
+                        if (error) {
+                            assert.deepStrictEqual(err, error);
+                            return next();
+                        }
+                        assert(!err);
+                        return next();
+                    }),
+                next => metadata.getObjectMD(bucketName, objectKey, {}, logger,
+                    (err, res) => {
+                        assert(!err);
+                        assert.deepStrictEqual(res, expectedVal);
+                        next();
+                    }),
+            ], done);
+        }));
+    });
+
+    describe('::deleteObjectWithCond', () => {
+        const tests = [
+            [
+                'should return no such key if the object does not exist',
+                {
+                    initVal: null,
+                    conditions: { value: { number: 24 } },
+                    expectedVal: null,
+                    error: errors.NoSuchKey,
+                },
+            ],
+            [
+                'should return no such key if the conditions fails',
+                {
+                    initVal: { value: { number: 0, string: 'zero' } },
+                    conditions: { value: { number: { $eq: 24 } } },
+                    expectedVal: { value: { number: 0, string: 'zero' } },
+                    error: errors.NoSuchKey,
+                },
+            ],
+            [
+                'should return no such key if the conditions fails',
+                {
+                    initVal: { value: { number: 0, string: 'zero' } },
+                    conditions: { value: { string: 'twenty-four' } },
+                    expectedVal: { value: { number: 0, string: 'zero' } },
+                    error: errors.NoSuchKey,
+                },
+            ],
+            [
+                'should return no such key if the conditions fails',
+                {
+                    initVal: { value: { number: 0, string: 'zero' } },
+                    conditions: {
+                        value: {
+                            string: 'twenty-four',
+                            number: { $eq: 0 },
+                        },
+                    },
+                    expectedVal: { value: { number: 0, string: 'zero' } },
+                    error: errors.NoSuchKey,
+                },
+            ],
+            [
+                'should successfully delete matched object',
+                {
+                    initVal: { value: { number: 24, string: 'twenty-four' } },
+                    conditions: { value: { number: 24 } },
+                    expectedVal: null,
+                    error: null,
+                },
+            ],
+            [
+                'should successfully delete matched object',
+                {
+                    initVal: { value: { number: 24, string: 'twenty-four' } },
+                    conditions: { value: { string: { $eq: 'twenty-four' } } },
+                    expectedVal: null,
+                    error: null,
+                },
+            ],
+            [
+                'should successfully delete matched object',
+                {
+                    initVal: { value: { number: 24, string: 'twenty-four' } },
+                    conditions: {
+                        value: {
+                            string: { $eq: 'twenty-four' },
+                            number: { $eq: 24 },
+                        },
+                    },
+                    expectedVal: null,
+                    error: null,
+                },
+            ],
+        ];
+        tests.forEach(([msg, testCase]) => it(msg, done => {
+            const objectKey = 'testkey';
+            const { initVal, conditions, expectedVal, error } = testCase;
+            const params = { conditions };
+            async.series([
+                next => {
+                    if (!initVal) {
+                        return next();
+                    }
+                    return metadata.putObjectMD(bucketName, objectKey, initVal,
+                                                {}, logger, next);
+                },
+                next => metadata.deleteObjectWithCond(bucketName, objectKey,
+                    params, logger, err => {
+                        if (error) {
+                            assert.deepStrictEqual(err, error);
+                            return next();
+                        }
+                        assert(!err);
+                        return next();
+                    }),
+                next => metadata.getObjectMD(bucketName, objectKey, {}, logger,
+                    (err, res) => {
+                        if (expectedVal) {
+                            assert.deepStrictEqual(res, expectedVal);
+                        } else {
+                            assert.deepStrictEqual(err, errors.NoSuchKey);
+                        }
+                        return next();
+                    }),
+            ], done);
+        }));
+    });
+});
--- tests/unit/storage/metadata/mongoclient/utils.js
+++ tests/unit/storage/metadata/mongoclient/utils.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 
+const errors = require('../../../../../lib/errors');
+
 const {
     credPrefix,
+    translateConditions,
 } = require('../../../../../lib/storage/metadata/mongoclient/utils');
 
 describe('auth credentials', () => {
@@ -24,4 +27,199 @@ describe('auth credentials', () => {
         };
         assert.strictEqual(credPrefix(creds), 'u%3A:%40p@');
     });
+});
+
+describe('translate query object', () => {
+    const tests = [
+        [
+            'should throw an error if depth < 0',
+            {
+                depth: -1,
+                prefix: '',
+                query: { $eq: 42 },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if depth > 10',
+            {
+                depth: 11,
+                prefix: '',
+                query: { $eq: 42 },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: null,
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: undefined,
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: [42],
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: { nested: null },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: {
+                    $eq: 42,
+                    nested: 42,
+                },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: {
+                    $eq: 42,
+                    nested: 21,
+                },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: {
+                    $eq: 42,
+                    $ne: 21,
+                },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should throw an error if query contains an invalid value',
+            {
+                depth: 0,
+                prefix: '',
+                query: {
+                    nested1: { $eq: 42 },
+                    nested2: null,
+                },
+                error: errors.InternalError,
+                result: null,
+            },
+        ],
+        [
+            'should return filter',
+            {
+                depth: 0,
+                prefix: 'prefix',
+                query: 42,
+                error: null,
+                result: { prefix: 42 },
+            },
+        ],
+        [
+            'should return filter',
+            {
+                depth: 0,
+                prefix: 'prefix',
+                query: { nested: 42 },
+                error: null,
+                result: { 'prefix.nested': 42 },
+            },
+        ],
+        [
+            'should return filter',
+            {
+                depth: 0,
+                prefix: '',
+                query: { nested: 42 },
+                error: null,
+                result: { nested: 42 },
+            },
+        ],
+        [
+            'should return filter',
+            {
+                depth: 0,
+                prefix: 'prefix',
+                query: { $eq: 42 },
+                error: null,
+                result: { prefix: { $eq: 42 } },
+            },
+        ],
+        [
+            'should return filter',
+            {
+                depth: 0,
+                prefix: 'prefix',
+                query: { nested: { $eq: 42 } },
+                error: null,
+                result: { 'prefix.nested': { $eq: 42 } },
+            },
+        ],
+        [
+            'should return filter',
+            {
+                depth: 0,
+                prefix: 'prefix',
+                query: {
+                    nested1: { $eq: 42 },
+                    nested2: 'forty-two',
+                },
+                error: null,
+                result: {
+                    'prefix.nested1': { $eq: 42 },
+                    'prefix.nested2': 'forty-two',
+                },
+            },
+        ],
+    ];
+    tests.forEach(([msg, params]) => it(msg, () => {
+        const { depth, prefix, query, error, result } = params;
+        if (error) {
+            assert.throws(
+                () => translateConditions(depth, prefix, {}, query),
+                error);
+            return;
+        }
+        const filter = {};
+        translateConditions(depth, prefix, filter, query);
+        assert.deepStrictEqual(filter, result);
+    }));
 });
