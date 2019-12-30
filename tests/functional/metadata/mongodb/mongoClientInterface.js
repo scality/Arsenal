239060765c40c7ee1b9c9733ebb548dcc01085cd
Alexander Chan <alexander.chan@scality.com>
2019-12-30 22:42:41 +0000

    [squash] changes

--- tests/functional/metadata/mongodb/mongoClientInterface.js
+++ tests/functional/metadata/mongodb/mongoClientInterface.js
@@ -1,11 +1,14 @@
-const assert = require('assert');
 const async = require('async');
+const assert = require('assert');
 const werelogs = require('werelogs');
 const { MongoMemoryReplSet } = require('mongodb-memory-server');
 
 const errors = require('../../../../lib/errors');
 const logger = new werelogs.Logger('MongoClientInterface', 'debug', 'debug');
 const BucketInfo = require('../../../../lib/models/BucketInfo');
+const ObjectMD = require('../../../../lib/models/ObjectMD');
+const MongoClientInterface =
+    require('../../../../lib/storage/metadata/mongoclient/MongoClientInterface');
 const MetadataWrapper =
     require('../../../../lib/storage/metadata/MetadataWrapper');
 
@@ -314,3 +317,331 @@ describe('MongoClientInterface', () => {
         }));
     });
 });
+
+function isTransientFn(locationName, log, callback) {
+    if (locationName.startsWith('transient')) {
+        return callback(null, true);
+    }
+    return callback(null, false);
+}
+
+function createBucketMD(locationName, bucketName, isVersioned, creationDate) {
+    const bucketObj = {
+        '_acl': {
+            'Canned': 'private',
+            'FULL_CONTROL': [],
+            'WRITE': [],
+            'WRITE_ACP': [],
+            'READ': [],
+            'READ_ACP': []
+        },
+        '_name': bucketName,
+        '_owner': 'testOwner',
+        '_ownerDisplayName':'testOwnerDisplayName',
+        '_creationDate': creationDate,
+        '_mdBucketModelVersion': 10,
+        '_transient': false,
+        '_deleted': false,
+        '_serverSideEncryption': null,
+        '_versioningConfiguration': isVersioned === null
+            ? null
+            : { Status: isVersioned ? 'Enabled' : 'Suspended' },
+        '_locationConstraint': locationName,
+        '_readLocationConstraint': null,
+        '_cors': null,
+        '_replicationConfiguration': null,
+        '_lifecycleConfiguration': null,
+        '_uid': '',
+        '_isNFS': null,
+        'ingestion': null
+    };
+    return BucketInfo.fromObj(bucketObj);
+}
+
+function createObjectMD(params) {
+    const {
+        locationName,
+        objectKey,
+        replicationInfo,
+        lastModified,
+        size,
+        versionId,
+    } = params;
+    return new ObjectMD()
+        .setLastModified(lastModified)
+        .setContentLength(size)
+        .setDataStoreName(locationName)
+        .setReplicationInfo(replicationInfo)
+        .setVersionId(versionId)
+        .setKey(objectKey);
+}
+
+function createReplicationInfo(backends, status) {
+    return {
+        status,
+        backends,
+        content: [],
+        destination: '',
+        storageClass: '',
+        role: '',
+        storageType: '',
+        dataStoreVersionId: '',
+        isNFS: null,
+    };
+}
+
+describe.only('MongoClientInterface::scanItemCount', () => {
+    let mongoc;
+    before(done => {
+        mongoserver.waitUntilRunning().then(() => {
+            const opts = {
+                isLocationTransient: isTransientFn,
+                replicaSetHosts: 'localhost:27018',
+                writeConcern: 'majority',
+                replicaSet: 'rs0',
+                readPreference: 'primary',
+                database: dbName,
+                logger
+            };
+            mongoc = new MongoClientInterface(opts);
+            mongoc.setup(done);
+        });
+    });
+
+    after(done => {
+        async.series([
+            next => mongoc.close(next),
+            next => mongoserver.stop()
+                .then(() => next())
+                .catch(next),
+        ], done);
+    });
+
+    it('should return correct count with transient location (COMPLETED)', done => {
+        const bucketName = 'test-bucket';
+        const location = 'transient-loc-1';
+        const createDate = new Date().toJSON();
+        const objectName = 'test-object';
+        const versionId = '00001';
+        const params = {
+            locationName: location,
+            objectKey: objectName,
+            replicationInfo: createReplicationInfo([
+                {site: 'loc1', status: 'COMPLETED'},
+                {site: 'loc2', status: 'COMPLETED'},
+            ], 'COMPLETED'),
+            size: 1,
+            versionId,
+            lastModified: new Date().toJSON(),
+        };
+        const expected = {
+            total: { curr: 2, prev: 0 },
+            byLocation: {
+                'transient-loc-1': { curr: 0, prev: 0 },
+                'loc1': { curr: 1, prev: 0 },
+                'loc2': { curr: 1, prev: 0 },
+            },
+        };
+        async.series([
+            next => mongoc.createBucket(
+                bucketName,
+                createBucketMD(location, bucketName, false, createDate),
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: false },
+                logger,
+                next),
+            next => scanAndTest(mongoc, expected, next),
+        ], done);
+    });
+
+    it('should return correct count with transient location (PENDING)', done => {
+        const bucketName = 'test-bucket';
+        const location = 'transient-loc-1';
+        const createDate = new Date().toJSON();
+        const objectName = 'test-object';
+        const versionId = '00001';
+        const params = {
+            locationName: location,
+            objectKey: objectName,
+            replicationInfo: createReplicationInfo([
+                {site: 'loc1', status: 'COMPLETED'},
+                {site: 'loc2', status: 'COMPLETED'},
+            ], 'PENDING'),
+            size: 1,
+            versionId,
+            lastModified: new Date().toJSON(),
+        };
+        const expected = {
+            total: { curr: 3, prev: 0 },
+            byLocation: {
+                'transient-loc-1': { curr: 1, prev: 0 },
+                'loc1': { curr: 1, prev: 0 },
+                'loc2': { curr: 1, prev: 0 },
+            },
+        };
+        async.series([
+            next => mongoc.createBucket(
+                bucketName,
+                createBucketMD(location, bucketName, false, createDate),
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: false },
+                logger,
+                next),
+            next => scanAndTest(mongoc, expected, next),
+        ], done);
+    });
+
+    it('should correctly count null objects', done => {
+        const bucketName = 'test-bucket';
+        const location = 'test-loc-1';
+        const createDate = new Date().toJSON();
+        const objectName = 'test-object';
+        const versionId = '00001';
+        const params = {
+            locationName: location,
+            objectKey: objectName,
+            replicationInfo: createReplicationInfo([], ''),
+            size: 1,
+            versionId,
+            lastModified: new Date().toJSON(),
+        };
+        const expected = {
+            total: { curr: 1, prev: 0 },
+            byLocation: {
+                'test-loc-1': { curr: 1, prev: 0 },
+            },
+        };
+        async.series([
+            next => mongoc.createBucket(
+                bucketName,
+                createBucketMD(location, bucketName, false, createDate),
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: false },
+                logger,
+                next),
+            next => mongoc.createBucket(
+                bucketName,
+                createBucketMD(location, bucketName, true, createDate),
+                logger,
+                next),
+            next => scanAndTest(mongoc, expected, next),
+        ], done);
+    });
+
+    it('should correctly count versioned objects', done => {
+        const bucketName = 'test-bucket';
+        const location = 'test-loc-1';
+        const createDate = new Date().toJSON();
+        const objectName = 'test-object';
+        const versionId = '00001';
+        const params = {
+            locationName: location,
+            objectKey: objectName,
+            replicationInfo: createReplicationInfo([], ''),
+            size: 1,
+            versionId,
+            lastModified: new Date().toJSON(),
+        };
+        const expected = {
+            total: { curr: 1, prev: 1 },
+            byLocation: {
+                'test-loc-1': { curr: 1, prev: 1 },
+            },
+        };
+        async.series([
+            next => mongoc.createBucket(
+                bucketName,
+                createBucketMD(location, bucketName, true, createDate),
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: true, versionId: '00001'},
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: true, versionId: '00002' },
+                logger,
+                next),
+            next => scanAndTest(mongoc, expected, next),
+        ], done);
+    });
+
+    it('should correctly count stalled objects', done => {
+        const bucketName = 'test-bucket';
+        const location = 'test-loc-1';
+        const createDate = new Date().toJSON();
+        const objectName = 'test-object';
+        const versionId = '00001';
+        const params = {
+            locationName: location,
+            objectKey: objectName,
+            replicationInfo: createReplicationInfo([], ''),
+            size: 1,
+            versionId,
+            lastModified: new Date().toJSON(),
+        };
+        const expected = {
+            total: { curr: 1, prev: 1 },
+            byLocation: {
+                'test-loc-1': { curr: 1, prev: 1 },
+            },
+        };
+        async.series([
+            next => mongoc.createBucket(
+                bucketName,
+                createBucketMD(location, bucketName, true, createDate),
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: true, versionId: '00001'},
+                logger,
+                next),
+            next => mongoc.putObject(
+                bucketName,
+                objectName,
+                createObjectMD(params).getValue(),
+                { versioning: true, versionId: '00002' },
+                logger,
+                next),
+            next => scanAndTest(mongoc, expected, next),
+        ], done);
+    });
+});
+
+function scanAndTest(mongoc, expected, callback) {
+    async.series([
+        next => mongoc.scanItemCount(logger, next),
+        next => mongoc.countItems(logger, (err, res) => {
+            if (err) {
+                return next(err);
+            }
+            console.log(res)
+            assert.deepStrictEqual(res.dataManaged, expected);
+            return next();
+        }),
+    ], callback);
+}
