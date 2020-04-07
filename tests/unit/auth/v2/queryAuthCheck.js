362f6cacf0e80024b44201cd8b141db47fa0e240
Ilke <ilke.akcay@scality.com>
2020-04-07 09:27:19 -0700

    Add unit tests for default preSignUrlExpiry value

--- tests/unit/auth/v2/queryAuthCheck.js
+++ tests/unit/auth/v2/queryAuthCheck.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const sinon = require('sinon');
 
 const queryAuthCheck =
     require('../../../../lib/auth/v2/queryAuthCheck').check;
@@ -25,4 +26,47 @@ describe('v2: queryAuthCheck', () => {
             assert.strictEqual(res.err.MissingSecurityHeader, true);
         }
     }));
+});
+
+describe('v2: queryAuthCheck', () => {
+    let clock = sinon.useFakeTimers();
+
+    it('S3 URL should not expire before 7 days with default preSignUrlExpiry', () => {
+        const currentTime = Date.now() / 1000;
+        const expires = currentTime + 604799;  // in seconds
+        const request = { method: 'GET', url: 'mockurl', query: { Expires: expires }, headers: { 'Content-MD5': 'c' } };
+        const data = { Expires: expires, AWSAccessKeyId: 'keyId', Signature: 'sign' };
+        const res = queryAuthCheck(request, log, data);
+        // assert.strictEqual(res.err, undefined);  // this is not easy to pass as it fails 
+        assert.notStrictEqual(res.err.AccessDenied, true);
+        assert.notStrictEqual(res.err.RequestTimeTooSkewed, true);
+
+    });
+
+    clock.tick(604800000);  // take time 7 days ahead
+
+    it('S3 URL should expire after 7 days with default preSignUrlExpiry', () => {
+        const currentTime = Date.now();
+        const request = { method: 'GET', query: { Expires: currentTime } };
+        const data = { Expires: currentTime };
+        const res = queryAuthCheck(request, log, data);
+        assert.notStrictEqual(res.err, undefined);
+        assert.strictEqual(res.err.AccessDenied, true);
+    });
+    it('should raise MissingSecurityHeader with invalid expires parameter', () => {
+        const request = { method: 'GET', query: { Expires: 'a string' } };
+        const data = { Expires: 'a string' };
+        const res = queryAuthCheck(request, log, data);
+        assert.notStrictEqual(res.err, undefined);
+        assert.strictEqual(res.err.MissingSecurityHeader, true);
+    });
+    it('should raise RequestTimeTooSkewed with current time > expires time', () => {
+        const request = { method: 'GET', query: { Expires: 0 } };
+        const data = { Expires: 0 };
+        const res = queryAuthCheck(request, log, data);
+        assert.notStrictEqual(res.err, undefined);
+        assert.strictEqual(res.err.RequestTimeTooSkewed, true);
+    });
+
+    clock.restore();
 });
