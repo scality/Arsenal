f0f4131e6a44b992984fe7f9363aefae1083c870
Ilke <ilke.akcay@scality.com>
2020-04-07 11:30:26 -0700

    fix linter errors

--- tests/unit/auth/v2/queryAuthCheck.js
+++ tests/unit/auth/v2/queryAuthCheck.js
@@ -12,40 +12,53 @@ describe('v2: queryAuthCheck', () => {
         { token: undefined, error: false },
         { token: 'invalid-token', error: true },
         { token: 'a'.repeat(128), error: false },
-    ].forEach(test => it(`test with token(${test.token})`, () => {
-        const request = { method: 'GET' };
-        const data = {
-            SecurityToken: test.token,
-        };
-        const res = queryAuthCheck(request, log, data);
-        if (test.error) {
-            assert.notStrictEqual(res.err, undefined);
-            assert.strictEqual(res.err.InvalidToken, true);
-        } else {
-            assert.notStrictEqual(res.err, undefined);
-            assert.strictEqual(res.err.MissingSecurityHeader, true);
-        }
-    }));
+    ].forEach(test => {
+        it(`test with token(${test.token})`, () => {
+            const request = { method: 'GET' };
+            const data = {
+                SecurityToken: test.token,
+            };
+            const res = queryAuthCheck(request, log, data);
+            if (test.error) {
+                assert.notStrictEqual(res.err, undefined);
+                assert.strictEqual(res.err.InvalidToken, true);
+            } else {
+                assert.notStrictEqual(res.err, undefined);
+                assert.strictEqual(res.err.MissingSecurityHeader, true);
+            }
+        });
+    });
 });
 
 describe('v2: queryAuthCheck', () => {
+    const mockRequest = {
+        method: 'GET',
+        url: 'mockurl',
+        query: {
+            Expires: expires
+        },
+        headers: {
+            'Content-MD5': 'c'
+        },
+    };
     let clock = sinon.useFakeTimers();
 
-    it('S3 URL should not expire before 7 days with default preSignUrlExpiry', () => {
+    it('URL shouldn\'t expire before 7 days with default expiry', () => {
         const currentTime = Date.now() / 1000;
         const expires = currentTime + 604799;  // in seconds
-        const request = { method: 'GET', url: 'mockurl', query: { Expires: expires }, headers: { 'Content-MD5': 'c' } };
-        const data = { Expires: expires, AWSAccessKeyId: 'keyId', Signature: 'sign' };
-        const res = queryAuthCheck(request, log, data);
-        // assert.strictEqual(res.err, undefined);  // this is not easy to pass as it fails 
+        const data = {
+            Expires: expires,
+            AWSAccessKeyId: 'keyId',
+            Signature: 'sign'
+        };
+        const res = queryAuthCheck(mockRequest, log, data);
         assert.notStrictEqual(res.err.AccessDenied, true);
         assert.notStrictEqual(res.err.RequestTimeTooSkewed, true);
-
     });
 
-    clock.tick(604800000);  // take time 7 days ahead
+    clock.tick(604800000);  // take time 604800000ms (7 days) ahead
 
-    it('S3 URL should expire after 7 days with default preSignUrlExpiry', () => {
+    it('URL should expire after 7 days with default preSignUrlExpiry', () => {
         const currentTime = Date.now();
         const request = { method: 'GET', query: { Expires: currentTime } };
         const data = { Expires: currentTime };
@@ -53,14 +66,14 @@ describe('v2: queryAuthCheck', () => {
         assert.notStrictEqual(res.err, undefined);
         assert.strictEqual(res.err.AccessDenied, true);
     });
-    it('should raise MissingSecurityHeader with invalid expires parameter', () => {
+    it('should raise MissingSecurityHeader with invalid expires param', () => {
         const request = { method: 'GET', query: { Expires: 'a string' } };
         const data = { Expires: 'a string' };
         const res = queryAuthCheck(request, log, data);
         assert.notStrictEqual(res.err, undefined);
         assert.strictEqual(res.err.MissingSecurityHeader, true);
     });
-    it('should raise RequestTimeTooSkewed with current time > expires time', () => {
+    it('should raise RequestTimeTooSkewed with current time > expiry', () => {
         const request = { method: 'GET', query: { Expires: 0 } };
         const data = { Expires: 0 };
         const res = queryAuthCheck(request, log, data);
