dc23a62217d5ba010a5011752e177894a39f9eb8
Ilke <ilke.akcay@scality.com>
2020-04-09 10:04:52 -0700

    Revert "fix linter errors"
    This reverts commit f0f4131e6a44b992984fe7f9363aefae1083c870.

--- tests/unit/auth/v2/queryAuthCheck.js
+++ tests/unit/auth/v2/queryAuthCheck.js
@@ -12,58 +12,40 @@ describe('v2: queryAuthCheck', () => {
         { token: undefined, error: false },
         { token: 'invalid-token', error: true },
         { token: 'a'.repeat(128), error: false },
-    ].forEach(test => {
-        it(`test with token(${test.token})`, () => {
-            const request = { method: 'GET' };
-            const data = {
-                SecurityToken: test.token,
-            };
-            const res = queryAuthCheck(request, log, data);
-            if (test.error) {
-                assert.notStrictEqual(res.err, undefined);
-                assert.strictEqual(res.err.InvalidToken, true);
-            } else {
-                assert.notStrictEqual(res.err, undefined);
-                assert.strictEqual(res.err.MissingSecurityHeader, true);
-            }
-        });
-    });
+    ].forEach(test => it(`test with token(${test.token})`, () => {
+        const request = { method: 'GET' };
+        const data = {
+            SecurityToken: test.token,
+        };
+        const res = queryAuthCheck(request, log, data);
+        if (test.error) {
+            assert.notStrictEqual(res.err, undefined);
+            assert.strictEqual(res.err.InvalidToken, true);
+        } else {
+            assert.notStrictEqual(res.err, undefined);
+            assert.strictEqual(res.err.MissingSecurityHeader, true);
+        }
+    }));
 });
 
 describe('v2: queryAuthCheck', () => {
-    let clock;
+    let clock = sinon.useFakeTimers();
 
-    beforeEach(() => {
-        clock = sinon.useFakeTimers();
-    });
-    afterEach(() => {
-        process.env.PRE_SIGN_URL_EXPIRY = 604800000;
-        clock.restore();
-    });
-    it('URL should not expire before 7 days with default expiry', () => {
+    it('S3 URL should not expire before 7 days with default preSignUrlExpiry', () => {
         const currentTime = Date.now() / 1000;
         const expires = currentTime + 604799;  // in seconds
-        const mockRequest = {
-            method: 'GET',
-            url: 'mockurl',
-            query: {
-                Expires: expires,
-            },
-            headers: {
-                'Content-MD5': 'c',
-            },
-        };
-        const data = {
-            Expires: expires,
-            AWSAccessKeyId: 'keyId',
-            Signature: 'sign',
-        };
-        const res = queryAuthCheck(mockRequest, log, data);
+        const request = { method: 'GET', url: 'mockurl', query: { Expires: expires }, headers: { 'Content-MD5': 'c' } };
+        const data = { Expires: expires, AWSAccessKeyId: 'keyId', Signature: 'sign' };
+        const res = queryAuthCheck(request, log, data);
+        // assert.strictEqual(res.err, undefined);  // this is not easy to pass as it fails 
         assert.notStrictEqual(res.err.AccessDenied, true);
         assert.notStrictEqual(res.err.RequestTimeTooSkewed, true);
+
     });
-    it('URL should expire after 7 days with default expiry', () => {
-        clock.tick(604800000);  // take time 604800000ms (7 days) ahead
+
+    clock.tick(604800000);  // take time 7 days ahead
+
+    it('S3 URL should expire after 7 days with default preSignUrlExpiry', () => {
         const currentTime = Date.now();
         const request = { method: 'GET', query: { Expires: currentTime } };
         const data = { Expires: currentTime };
@@ -71,43 +53,15 @@ describe('v2: queryAuthCheck', () => {
         assert.notStrictEqual(res.err, undefined);
         assert.strictEqual(res.err.AccessDenied, true);
     });
-    it('URL should not expire before 7 days with custom expiry', () => {
-        process.env.PRE_SIGN_URL_EXPIRY = 31556952000; // in ms (1 year)
-        const currentTime = Date.now() / 1000;
-        const expires = currentTime + 604799;  // in seconds
-        const mockRequest = {
-            method: 'GET',
-            url: 'mockurl',
-            query: {
-                Expires: expires,
-            },
-            headers: {
-                'Content-MD5': 'c',
-            },
-        };
-        const data = {
-            Expires: expires,
-            AWSAccessKeyId: 'keyId',
-            Signature: 'sign',
-        };
-        const res = queryAuthCheck(mockRequest, log, data);
-        assert.notStrictEqual(res.err.AccessDenied, true);
-        assert.notStrictEqual(res.err.RequestTimeTooSkewed, true);
-    });
-    it('URL should still not expire after 7 days with custom expiry', () => {
-        clock.tick(604800000);  // take time 604800000ms (7 days) ahead
-        process.env.PRE_SIGN_URL_EXPIRY = 31556952000; // in ms (1 year)
-        const currentTime = Date.now() / 1000;
-        const request = { method: 'GET', query: { Expires: currentTime } };
-        const data = { Expires: currentTime };
+    it('should raise MissingSecurityHeader with invalid expires parameter', () => {
+        const request = { method: 'GET', query: { Expires: 'a string' } };
+        const data = { Expires: 'a string' };
         const res = queryAuthCheck(request, log, data);
         assert.notStrictEqual(res.err.AccessDenied, true);
     });
-    it('should return RequestTimeTooSkewed with current time > expiry', () => {
-        clock.tick(123);
-        const expires = 0;
-        const request = { method: 'GET', query: { Expires: expires } };
-        const data = { Expires: expires };
+    it('should raise RequestTimeTooSkewed with current time > expires time', () => {
+        const request = { method: 'GET', query: { Expires: 0 } };
+        const data = { Expires: 0 };
         const res = queryAuthCheck(request, log, data);
         assert.notStrictEqual(res.err, undefined);
         assert.strictEqual(res.err.RequestTimeTooSkewed, true);
