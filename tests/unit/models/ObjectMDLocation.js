1978405fb93c28c158e1c89516a0f06328367187
Jonathan Gramain <jonathan.gramain@scality.com>
2021-07-20 14:41:58 -0700

    improvement: S3C-4312 backport + adapt ObjectMDLocation unit test
    Backport and adapt to 7.x branch the ObjectMDLocation unit tests from
    development/8.1 branch

---
+++ tests/unit/models/ObjectMDLocation.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const ObjectMDLocation = require('../../../lib/models/ObjectMDLocation');
+
+describe('ObjectMDLocation', () => {
+    it('class getters/setters', () => {
+        const locValue = {
+            key: 'fookey',
+            start: 42,
+            size: 100,
+            dataStoreName: 'awsbackend',
+            dataStoreETag: '2:abcdefghi',
+            cryptoScheme: 1,
+            cipheredDataKey: 'CiPhErEdDaTaKeY',
+        };
+        const location = new ObjectMDLocation(locValue);
+        assert.strictEqual(location.getKey(), 'fookey');
+        assert.strictEqual(location.getDataStoreName(), 'awsbackend');
+        assert.strictEqual(location.getDataStoreETag(), '2:abcdefghi');
+        assert.strictEqual(location.getPartNumber(), 2);
+        assert.strictEqual(location.getPartETag(), 'abcdefghi');
+        assert.strictEqual(location.getPartStart(), 42);
+        assert.strictEqual(location.getPartSize(), 100);
+        assert.strictEqual(location.getCryptoScheme(), 1);
+        assert.strictEqual(location.getCipheredDataKey(), 'CiPhErEdDaTaKeY');
+
+        assert.deepStrictEqual(location.getValue(), locValue);
+
+        location.setPartSize(200);
+        assert.strictEqual(location.getPartSize(), 200);
+    });
+
+    it('ObjectMDLocation::setDataLocation()', () => {
+        const location = new ObjectMDLocation({
+            key: 'fookey',
+            start: 42,
+            size: 100,
+            dataStoreName: 'awsbackend',
+            dataStoreETag: '2:abcdefghi',
+            cryptoScheme: 1,
+            cipheredDataKey: 'CiPhErEdDaTaKeY',
+        });
+        location.setDataLocation({ key: 'secondkey',
+                                   dataStoreName: 'gcpbackend' });
+        assert.strictEqual(location.getKey(), 'secondkey');
+        assert.strictEqual(location.getDataStoreName(), 'gcpbackend');
+        assert.strictEqual(location.getCryptoScheme(), undefined);
+        assert.strictEqual(location.getCipheredDataKey(), undefined);
+        location.setDataLocation({ key: 'thirdkey',
+                                   dataStoreName: 'azurebackend',
+                                   cryptoScheme: 1,
+                                   cipheredDataKey: 'NeWcIpHeReDdAtAkEy' });
+        assert.strictEqual(location.getKey(), 'thirdkey');
+        assert.strictEqual(location.getDataStoreName(), 'azurebackend');
+        assert.strictEqual(location.getCryptoScheme(), 1);
+        assert.strictEqual(location.getCipheredDataKey(), 'NeWcIpHeReDdAtAkEy');
+    });
+});
