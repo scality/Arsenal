9aa8710a57b037685ad2851543808df3a41476b2
Jonathan Gramain <jonathan.gramain@scality.com>
2021-08-03 17:53:45 -0700

    ARSN-9 KMIP deep healthcheck
    Add a healthcheck() function in the KMIP client that create a dummy
    bucket key on the KMS then deletes it, to ensure basic functionality
    is working

--- tests/functional/kmip/highlevel.js
+++ tests/functional/kmip/highlevel.js
@@ -8,6 +8,8 @@ const LoopbackServerChannel =
       require('../../utils/kmip/LoopbackServerChannel.js');
 const TransportTemplate =
       require('../../../lib/network/kmip/transport/TransportTemplate.js');
+const TlsTransport =
+      require('../../../lib/network/kmip/transport/tls.js');
 const KMIP = require('../../../lib/network/kmip');
 const KMIPClient = require('../../../lib/network/kmip/Client.js');
 const {
@@ -63,6 +65,54 @@ describe('KMIP High Level Driver', () => {
                            kmipClient.destroyBucketKey(id, logger, next),
                    ], done);
                });
+        });
+    });
+    it('should succeed healthcheck with working KMIP client and server', done => {
+        const options = {
+            kmip: {
+                client: {
+                    bucketNameAttributeName: null,
+                    compoundCreateActivate: false,
+                },
+                codec: {},
+                transport: {
+                    pipelineDepth: 8,
+                    tls: {
+                        port: 5696,
+                    },
+                },
+            },
+        };
+        const kmipClient = new KMIPClient(options, TTLVCodec,
+                                          LoopbackServerTransport);
+        kmipClient.healthcheck(logger, err => {
+            assert.ifError(err);
+            done();
+        });
+    });
+
+    it('should fail healthcheck with KMIP server not running', done => {
+        const options = {
+            kmip: {
+                client: {
+                    bucketNameAttributeName: null,
+                    compoundCreateActivate: false,
+                },
+                codec: {},
+                transport: {
+                    pipelineDepth: 8,
+                    tls: {
+                        port: 5696,
+                    },
+                },
+            },
+        };
+        const kmipClient = new KMIPClient(options, TTLVCodec, TlsTransport);
+        kmipClient.healthcheck(logger, err => {
+            assert(err);
+            assert(err.InternalError);
+            assert(err.description.includes('ECONNREFUSED'));
+            done();
         });
     });
 });
