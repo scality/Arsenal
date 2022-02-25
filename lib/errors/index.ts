48cf48c8ebecacdb8436bebfbd67527bec487b6c
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-02-25 17:02:22 +0100

    Refactor v2

--- lib/errors/index.ts
+++ lib/errors/index.ts
@@ -4,55 +4,89 @@ import * as types from './types';
 
 export * from './types';
 
-export class ArsenalError extends Error {
-    code: number;
-    description: string;
-    type: types.Name;
+/** Mapping used to determine an error type. */
+export type Is = { [Name in types.Name]: boolean };
+/** Mapping of all possible Errors */
+export type Errors = { [Property in keyof types.Names]: ArsenalError };
 
-    constructor(type: types.Name, code: number, description: string) {
+const entries = Object.entries(rawErrors);
+
+// This contains some metaprog. Be careful.
+const createIs = (type: types.Name) => {
+    const get = (_: {}, value: string | symbol) => type === value;
+    return new Proxy({}, { get }) as Is;
+};
+
+export class ArsenalError extends Error {
+    /** HTTP status code. Example: 401, 403, 500, ... */
+    #code: number;
+    /** Text description of the error. */
+    #description: string;
+    /** Type of the error. Belongs to errors.types. */
+    #type: types.Name;
+    /** Object used to determine the error type.
+     * Example: error.is.InternalError */
+    #is: Is;
+
+    private constructor(type: types.Name, code: number, description: string) {
         super(type);
-        this.code = code;
-        this.description = description;
-        this.type = type;
+        this.#code = code;
+        this.#description = description;
+        this.#type = type;
+        this.#is = createIs(type);
     }
 
     /** Output the error as a JSON string */
     toString() {
-        const { message, description } = this;
-        return JSON.stringify({
-            errorType: message,
-            errorMessage: description,
-        });
+        const errorType = this.#type;
+        const errorMessage = this.#description;
+        return JSON.stringify({ errorType, errorMessage });
     }
 
     /** Write the error in an HTTP response */
     writeResponse(res: http.ServerResponse) {
-        res.writeHead(this.code);
+        res.writeHead(this.#code);
         const asStr = this.toString();
         res.end(asStr);
     }
 
     /** Clone the error with a new description.*/
     customizeDescription(description: string): ArsenalError {
-        const { type, code } = this;
+        const type = this.#type;
+        const code = this.#code;
         return new ArsenalError(type, code, description);
     }
 
-    is(type: types.Name) {
-        return this.type === type;
+    get is() {
+        return this.#is;
+    }
+
+    get code() {
+        return this.#code;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get type() {
+        return this.#type;
+    }
+
+    /** Generate all possible errors. An instance is created by default. */
+    static errors() {
+        return entries.reduce((acc, value) => {
+            const name = value[0] as types.Name;
+            const error = value[1];
+            const { code, description } = error;
+            const err = new ArsenalError(name, code, description);
+            return { ...acc, [name]: err };
+        }, {} as Errors);
     }
 }
 
-/** Mapping of all possible Errors */
-export type Errors = { [Property in keyof types.Names]: ArsenalError };
-
 /** Mapping of all possible Errors.
  * Use them with errors[error].customizeDescription for any customization. */
-const errors = Object.entries(rawErrors).reduce((acc, value) => {
-    const name = value[0] as types.Name;
-    const error = value[1];
-    const { code, description } = error;
-    return { ...acc, [name]: new ArsenalError(name, code, description) };
-}, {} as Errors);
+const errors = ArsenalError.errors();
 
 export default errors;
