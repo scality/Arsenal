841d4e48236a69bcd3183a81f55d43830364bb0c
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-01 17:28:11 +0100

    Convert AuthLoader.spec to TS

--- tests/unit/auth/in_memory/AuthLoader.spec.ts
+++ tests/unit/auth/in_memory/AuthLoader.spec.ts
@@ -1,9 +1,11 @@
-const assert = require('assert');
-const werelogs = require('werelogs');
+import * as assert from 'assert';
+import * as werelogs from 'werelogs';
+import * as arsenal from '../../../../lib/auth/auth';
+const { AuthLoader } = arsenal.inMemory;
 
-const AuthLoader = require('../../../../lib/auth/auth').inMemory.AuthLoader;
 const ref = require('./sample_authdata.json');
 
+// @ts-ignore
 werelogs.configure({
     level: 'info',
     dump: 'error',
@@ -44,7 +46,7 @@ function shouldSucceed(obj, done) {
     done();
 }
 
-const should = {
+export const should = {
     _exec: undefined,
     missingField: (obj, field, done) => {
         delete getParentField(obj, field)[getFieldName(field)];
@@ -59,7 +61,7 @@ const should = {
 describe('AuthLoader class', () => {
     let obj = {};
 
-    beforeEach(done => {
+    beforeEach((done) => {
         obj = JSON.parse(JSON.stringify(ref));
         done();
     });
@@ -78,21 +80,20 @@ describe('AuthLoader class', () => {
         ['accounts.0.canonicalID', 64],
         ['accounts.0.keys', 'not an Array'],
         ['accounts.0.keys', undefined],
-    ].forEach(test => {
+    ].forEach((test) => {
         if (test[1] === undefined) {
             // Check a failure when deleting required fields
-            it(`should fail when missing field ${test[0]}`, done => {
+            it(`should fail when missing field ${test[0]}`, (done) => {
                 should._exec = shouldFail;
                 should.missingField(obj, test[0], done);
             });
         } else {
             // Check a failure when the type of field is different than
             // expected
-            it(`should fail when modified field ${test[0]} ${test[1]}`,
-                done => {
-                    should._exec = shouldFail;
-                    should.modifiedField(obj, test[0], test[1], done);
-                });
+            it(`should fail when modified field ${test[0]} ${test[1]}`, (done) => {
+                should._exec = shouldFail;
+                should.modifiedField(obj, test[0], test[1], done);
+            });
         }
     });
 
@@ -100,38 +101,31 @@ describe('AuthLoader class', () => {
     // optionals fields
     // format:
     //   - key: field to modify
-    [
-        'accounts.0.keys',
-        'accounts.0.users',
-    ].forEach(test => {
+    ['accounts.0.keys', 'accounts.0.users'].forEach((test) => {
         // Check a success when deleting optional fields
-        it(`should return success when missing field ${test}`, done => {
+        it(`should return success when missing field ${test}`, (done) => {
             should._exec = shouldSucceed;
             should.missingField(obj, test[0], done);
         });
     });
 
-    it('Should return error on two same canonicalID', done => {
+    it('Should return error on two same canonicalID', (done) => {
         obj.accounts[0].canonicalID = obj.accounts[1].canonicalID;
         shouldFail(obj, done);
     });
 
-    it('Should return error on two same emails', done => {
+    it('Should return error on two same emails', (done) => {
         obj.accounts[0].email = obj.accounts[1].email;
         shouldFail(obj, done);
     });
 
-    it('Should return error on two same arn', done => {
+    it('Should return error on two same arn', (done) => {
         obj.accounts[0].arn = obj.accounts[1].arn;
         shouldFail(obj, done);
     });
 
-    it('Should return error on two same access key', done => {
+    it('Should return error on two same access key', (done) => {
         obj.accounts[0].keys[0].access = obj.accounts[1].keys[0].access;
         shouldFail(obj, done);
     });
 });
-
-module.exports = {
-    should,
-};
