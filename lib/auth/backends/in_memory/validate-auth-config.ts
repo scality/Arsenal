f4fcc4b773b032993c97d6bece66e76c4aad24e3
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-01 18:22:27 +0100

    Refactor validate-auth-config with types

--- lib/auth/backends/in_memory/validate-auth-config.ts
+++ lib/auth/backends/in_memory/validate-auth-config.ts
@@ -1,15 +1,17 @@
+import { Logger } from 'werelogs';
 import AuthLoader from './AuthLoader';
+import { Accounts } from './types';
 
 /**
  * @deprecated please use {@link AuthLoader} class instead
- *
- * @param {object} authdata - the authentication config file's data
- * @param {werelogs.API} logApi - object providing a constructor function
- *                                for the Logger object
- * @return {boolean} true on erroneous data
- *                   false on success
+ * @param authdata - the authentication config file's data
+ * @param logApi - object providing a constructor function for the Logger object
+ * @return true on erroneous data false on success
  */
-export default function validateAuthConfig(authdata, logApi) {
+export default function validateAuthConfig(
+    authdata: Accounts,
+    logApi?: { Logger: typeof Logger }
+) {
     const authLoader = new AuthLoader(logApi);
     authLoader.addAccounts(authdata);
     return !authLoader.validate();
