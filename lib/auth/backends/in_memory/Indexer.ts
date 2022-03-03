4b76407eddb119a9c06462523f1f0107df799b8b
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-03 14:03:01 +0100

    Type Indexer

--- lib/auth/backends/in_memory/Indexer.ts
+++ lib/auth/backends/in_memory/Indexer.ts
@@ -1,27 +1,19 @@
+import { Accounts, Account, Entity } from './types';
+
 /**
  * Class that provides an internal indexing over the simple data provided by
  * the authentication configuration file for the memory backend. This allows
  * accessing the different authentication entities through various types of
  * keys.
- *
- * @class Indexer
  */
 export default class Indexer {
-    /**
-     * @constructor
-     * @param {object} authdata - the authentication config file's data
-     * @param {object[]} authdata.accounts - array of account objects
-     * @param {string=} authdata.accounts[].name - account name
-     * @param {string} authdata.accounts[].email - account email
-     * @param {string} authdata.accounts[].arn - IAM resource name
-     * @param {string} authdata.accounts[].canonicalID - account canonical ID
-     * @param {string} authdata.accounts[].shortid - short account ID
-     * @param {object[]=} authdata.accounts[].keys - array of key objects
-     * @param {string} authdata.accounts[].keys[].access - access key
-     * @param {string} authdata.accounts[].keys[].secret - secret key
-     * @return {undefined}
-     */
-    constructor(authdata) {
+    accountsBy: {
+        canId: { [id: string]: Entity | undefined },
+        accessKey: { [id: string]: Entity | undefined },
+        email: { [id: string]: Entity | undefined },
+    }
+
+    constructor(authdata?: Accounts) {
         this.accountsBy = {
             canId: {},
             accessKey: {},
@@ -37,11 +29,11 @@ export default class Indexer {
             return;
         }
 
-        this._build(authdata);
+        this.#build(authdata);
     }
 
-    _indexAccount(account) {
-        const accountData = {
+    #indexAccount(account: Account) {
+        const accountData: Entity = {
             arn: account.arn,
             canonicalID: account.canonicalID,
             shortid: account.shortid,
@@ -59,85 +51,43 @@ export default class Indexer {
         }
     }
 
-    _build(authdata) {
+    #build(authdata: Accounts) {
         authdata.accounts.forEach(account => {
-            this._indexAccount(account);
+            this.#indexAccount(account);
         });
     }
 
-    /**
-     * This method returns the account associated to a canonical ID.
-     *
-     * @param {string} canId - The canonicalId of the account
-     * @return {Object} account - The account object
-     * @return {Object} account.arn - The account's ARN
-     * @return {Object} account.canonicalID - The account's canonical ID
-     * @return {Object} account.shortid - The account's internal shortid
-     * @return {Object} account.accountDisplayName - The account's display name
-     * @return {Object} account.email - The account's lowercased email
-     */
-    getEntityByCanId(canId) {
+    /** This method returns the account associated to a canonical ID. */
+    getEntityByCanId(canId: string): Entity | undefined {
         return this.accountsBy.canId[canId];
     }
 
     /**
      * This method returns the entity (either an account or a user) associated
      * to a canonical ID.
-     *
      * @param {string} key - The accessKey of the entity
-     * @return {Object} entity - The entity object
-     * @return {Object} entity.arn - The entity's ARN
-     * @return {Object} entity.canonicalID - The canonical ID for the entity's
-     *                                       account
-     * @return {Object} entity.shortid - The entity's internal shortid
-     * @return {Object} entity.accountDisplayName - The entity's account
-     *                                              display name
-     * @return {Object} entity.IAMDisplayName - The user's display name
-     *                                          (if the entity is an user)
-     * @return {Object} entity.email - The entity's lowercased email
      */
-    getEntityByKey(key) {
+    getEntityByKey(key: string): Entity | undefined {
         return this.accountsBy.accessKey[key];
     }
 
     /**
      * This method returns the entity (either an account or a user) associated
      * to an email address.
-     *
-     * @param {string} email - The email address
-     * @return {Object} entity - The entity object
-     * @return {Object} entity.arn - The entity's ARN
-     * @return {Object} entity.canonicalID - The canonical ID for the entity's
-     *                                       account
-     * @return {Object} entity.shortid - The entity's internal shortid
-     * @return {Object} entity.accountDisplayName - The entity's account
-     *                                              display name
-     * @return {Object} entity.IAMDisplayName - The user's display name
-     *                                          (if the entity is an user)
-     * @return {Object} entity.email - The entity's lowercased email
      */
-    getEntityByEmail(email) {
+    getEntityByEmail(email: string): Entity | undefined {
         const lowerCasedEmail = email.toLowerCase();
         return this.accountsBy.email[lowerCasedEmail];
     }
 
-    /**
-    * This method returns the secret key associated with the entity.
-    * @param {Object} entity - the entity object
-    * @param {string} accessKey - access key
-    * @returns {string} secret key
-    */
-    getSecretKey(entity, accessKey) {
-        return entity.keys
-            .filter(kv => kv.access === accessKey)[0].secret;
+    /** This method returns the secret key associated with the entity. */
+    getSecretKey(entity: Entity, accessKey: string) {
+        const keys = entity.keys.filter(kv => kv.access === accessKey);
+        return keys[0].secret;
     }
 
-    /**
-    * This method returns the account display name associated with the entity.
-    * @param {Object} entity - the entity object
-    * @returns {string} account display name
-    */
-    getAcctDisplayName(entity) {
+    /** This method returns the account display name associated with the entity. */
+    getAcctDisplayName(entity: Entity) {
         return entity.accountDisplayName;
     }
 }
