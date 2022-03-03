8f4870f5a24bcedabc8f7ddd541eada60b852c1f
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-03 14:03:36 +0100

    Type ChainBackend

--- lib/auth/backends/ChainBackend.ts
+++ lib/auth/backends/ChainBackend.ts
@@ -1,25 +1,31 @@
 import assert from 'assert';
 import async from 'async';
 
+import { Callback } from './in_memory/types';
 import errors from '../../errors';
 import BaseBackend from './BaseBackend';
+
+export type Policy = {
+    [key: string]: any;
+    arn?: string;
+    versionId?: string;
+    isAllowed: boolean;
+}
 
 /**
  * Class that provides an authentication backend that will verify signatures
  * and retrieve emails and canonical ids associated with an account using a
  * given list of authentication backends and vault clients.
- *
- * @class ChainBackend
  */
 export default class ChainBackend extends BaseBackend {
-    _clients: any[];
+    #clients: BaseBackend[];
 
     /**
      * @constructor
      * @param {string} service - service id
      * @param {object[]} clients - list of authentication backends or vault clients
      */
-    constructor(service: string, clients: any[]) {
+    constructor(service: string, clients: BaseBackend[]) {
         super(service);
 
         assert(Array.isArray(clients) && clients.length > 0, 'invalid client list');
@@ -31,25 +37,28 @@ export default class ChainBackend extends BaseBackend {
             typeof client.checkPolicies === 'function' &&
             typeof client.healthcheck === 'function'
         ), 'invalid client: missing required auth backend methods');
-        this._clients = clients;
+        this.#clients = clients;
     }
 
-    /*
-     * try task against each client for one to be successful
-     */
-    _tryEachClient(task, cb) {
-        async.tryEach(this._clients.map(client => done => task(client, done)), cb);
+    /** try task against each client for one to be successful */
+    #tryEachClient(task: (client: BaseBackend, done?: any) => void, cb: Callback) {
+        // @ts-ignore
+        async.tryEach(this.#clients.map(client => (done: any) => task(client, done)), cb);
     }
 
-    /*
-     * apply task to all clients
-     */
-    _forEachClient(task, cb) {
-        async.map(this._clients, task, cb);
+    /** apply task to all clients */
+    #forEachClient(task: (client: BaseBackend, done?: any) => void, cb: Callback) {
+        async.map(this.#clients, task, cb);
     }
 
-    verifySignatureV2(stringToSign, signatureFromRequest, accessKey, options, callback) {
-        this._tryEachClient((client, done) => client.verifySignatureV2(
+    verifySignatureV2(
+        stringToSign: string,
+        signatureFromRequest: string,
+        accessKey: string,
+        options: any,
+        callback: Callback
+    ) {
+        this.#tryEachClient((client, done) => client.verifySignatureV2(
             stringToSign,
             signatureFromRequest,
             accessKey,
@@ -58,27 +67,39 @@ export default class ChainBackend extends BaseBackend {
         ), callback);
     }
 
-    verifySignatureV4(stringToSign, signatureFromRequest, accessKey, region, scopeDate, options, callback) {
-        this._tryEachClient((client, done) => client.verifySignatureV4(
+    verifySignatureV4(
+        stringToSign: string,
+        signatureFromRequest: string,
+        accessKey: string,
+        region: string,
+        scopeDate: string,
+        options: any,
+        callback: Callback
+    ) {
+        this.#tryEachClient((client, done) => client.verifySignatureV4(
             stringToSign,
-                signatureFromRequest,
-                accessKey,
-                region,
-                scopeDate,
-                options,
-                done
+            signatureFromRequest,
+            accessKey,
+            region,
+            scopeDate,
+            options,
+            done
         ), callback);
     }
 
-    static _mergeObjects(objectResponses) {
+    static _mergeObjects(objectResponses: any[]) {
         return objectResponses.reduce(
             (retObj, resObj) => Object.assign(retObj, resObj.message.body),
             {}
         );
     }
 
-    getCanonicalIds(emailAddresses, options, callback) {
-        this._forEachClient(
+    getCanonicalIds(
+        emailAddresses: string[],
+        options: any,
+        callback: Callback<{ message: { body: any } }>
+    ) {
+        this.#forEachClient(
             (client, done) => client.getCanonicalIds(emailAddresses, options, done),
             (err, res) => {
                 if (err) {
@@ -94,8 +115,12 @@ export default class ChainBackend extends BaseBackend {
         );
     }
 
-    getEmailAddresses(canonicalIDs, options, callback) {
-        this._forEachClient(
+    getEmailAddresses(
+        canonicalIDs: string[],
+        options: any,
+        callback: Callback<{ message: { body: any } }>
+    ) {
+        this.#forEachClient(
             (client, done) => client.getEmailAddresses(canonicalIDs, options, done),
             (err, res) => {
                 if (err) {
@@ -110,11 +135,9 @@ export default class ChainBackend extends BaseBackend {
         );
     }
 
-    /*
-     * merge policy responses into a single message
-     */
-    static _mergePolicies(policyResponses) {
-        const policyMap = {};
+    /** merge policy responses into a single message */
+    static _mergePolicies(policyResponses: { message: { body: any[] } }[]) {
+        const policyMap: { [key: string]: Policy } = {};
 
         policyResponses.forEach(resp => {
             if (!resp.message || !Array.isArray(resp.message.body)) {
@@ -131,8 +154,8 @@ export default class ChainBackend extends BaseBackend {
         });
 
         return Object.keys(policyMap).map((key) => {
-            const policyRes = { isAllowed: policyMap[key].isAllowed };
-            if (policyMap[key].arn !== '') {
+            const policyRes: Policy = { isAllowed: policyMap[key].isAllowed };
+            if (policyMap[key].arn && policyMap[key].arn !== '') {
                 policyRes.arn = policyMap[key].arn;
             }
             if (policyMap[key].versionId) {
@@ -142,7 +165,7 @@ export default class ChainBackend extends BaseBackend {
         });
     }
 
-    /*
+    /**
         response format:
             {  message: {
                 body: [{}],
@@ -150,8 +173,13 @@ export default class ChainBackend extends BaseBackend {
                 message: string,
             } }
      */
-    checkPolicies(requestContextParams, userArn, options, callback) {
-        this._forEachClient((client, done) => client.checkPolicies(
+    checkPolicies(
+        requestContextParams: any,
+        userArn: string,
+        options: any,
+        callback: Callback<{ message: { body: any } }>
+    ) {
+        this.#forEachClient((client, done) => client.checkPolicies(
             requestContextParams,
             userArn,
             options,
@@ -168,8 +196,8 @@ export default class ChainBackend extends BaseBackend {
         });
     }
 
-    healthcheck(reqUid, callback) {
-        this._forEachClient((client, done) =>
+    healthcheck(reqUid: string, callback: Callback) {
+        this.#forEachClient((client, done) =>
             client.healthcheck(reqUid, (err, res) => done(null, {
                 error: !!err ? err : null,
                 status: res,
@@ -179,7 +207,7 @@ export default class ChainBackend extends BaseBackend {
                 return callback(err);
             }
 
-            const isError = res.some((results) => !!results.error);
+            const isError = res.some((results: any) => !!results.error);
             if (isError) {
                 return callback(errors.InternalError, res);
             }
