dc0a9bcd60cc023ab4d2d379fc904d0700a0a3cf
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-22 11:51:07 +0100

    ARSN-100 Get changes from index.js to index.ts

--- index.ts
+++ index.ts
@@ -14,15 +14,10 @@ export const https = {
 };
 
 export const algorithms = {
-    list: {
-        Basic: require('./lib/algos/list/basic').List,
-        Delimiter: require('./lib/algos/list/delimiter').Delimiter,
-        DelimiterVersions: require('./lib/algos/list/delimiterVersions').DelimiterVersions,
-        DelimiterMaster: require('./lib/algos/list/delimiterMaster').DelimiterMaster,
-        MPU: require('./lib/algos/list/MPU').MultipartUploads,
-    },
+    list: require('./lib/algos/list/exportAlgos'),
     listTools: {
         DelimiterTools: require('./lib/algos/list/tools'),
+        Skip: require('./lib/algos/list/skip'),
     },
     cache: {
         LRUCache: require('./lib/algos/cache/LRUCache'),
@@ -35,7 +30,8 @@ export const algorithms = {
 
 export const policies = {
     evaluators: require('./lib/policyEvaluator/evaluator.js'),
-    validateUserPolicy: require('./lib/policy/policyValidator').validateUserPolicy,
+    validateUserPolicy: require('./lib/policy/policyValidator')
+        .validateUserPolicy,
     evaluatePrincipal: require('./lib/policyEvaluator/principal'),
     RequestContext: require('./lib/policyEvaluator/RequestContext.js'),
     requestUtils: require('./lib/policyEvaluator/requestUtils'),
@@ -47,7 +43,8 @@ export const testing = {
 };
 
 export const versioning = {
-    VersioningConstants: require('./lib/versioning/constants.js').VersioningConstants,
+    VersioningConstants: require('./lib/versioning/constants.js')
+        .VersioningConstants,
     Version: require('./lib/versioning/Version.js').Version,
     VersionID: require('./lib/versioning/VersionID.js'),
     WriteGatheringManager: require('./lib/versioning/WriteGatheringManager.js'),
@@ -58,6 +55,7 @@ export const versioning = {
 export const network = {
     http: {
         server: require('./lib/network/http/server'),
+        utils: require('./lib/network/http/utils'),
     },
     rpc: require('./lib/network/rpc/rpc'),
     level: require('./lib/network/rpc/level-net'),
@@ -67,6 +65,9 @@ export const network = {
     },
     probe: {
         ProbeServer: require('./lib/network/probe/ProbeServer'),
+        HealthProbeServer:
+            require('./lib/network/probe/HealthProbeServer.js'),
+        Utils: require('./lib/network/probe/Utils.js'),
     },
     RoundRobin: require('./lib/network/RoundRobin'),
     kmip: require('./lib/network/kmip'),
@@ -84,46 +85,112 @@ export const s3middleware = {
     escapeForXml: require('./lib/s3middleware/escapeForXml'),
     objectLegalHold: require('./lib/s3middleware/objectLegalHold'),
     tagging: require('./lib/s3middleware/tagging'),
+    checkDateModifiedHeaders:
+        require('./lib/s3middleware/validateConditionalHeaders')
+            .checkDateModifiedHeaders,
     validateConditionalHeaders:
-        require('./lib/s3middleware/validateConditionalHeaders').validateConditionalHeaders,
+        require('./lib/s3middleware/validateConditionalHeaders')
+            .validateConditionalHeaders,
     MD5Sum: require('./lib/s3middleware/MD5Sum'),
     NullStream: require('./lib/s3middleware/nullStream'),
     objectUtils: require('./lib/s3middleware/objectUtils'),
     azureHelper: {
-        mpuUtils: require('./lib/s3middleware/azureHelpers/mpuUtils'),
-        ResultsCollector: require('./lib/s3middleware/azureHelpers/ResultsCollector'),
-        SubStreamInterface: require('./lib/s3middleware/azureHelpers/SubStreamInterface'),
+        mpuUtils:
+            require('./lib/s3middleware/azureHelpers/mpuUtils'),
+        ResultsCollector:
+            require('./lib/s3middleware/azureHelpers/ResultsCollector'),
+        SubStreamInterface:
+            require('./lib/s3middleware/azureHelpers/SubStreamInterface'),
     },
+    prepareStream: require('./lib/s3middleware/prepareStream'),
+    processMpuParts: require('./lib/s3middleware/processMpuParts'),
     retention: require('./lib/s3middleware/objectRetention'),
     lifecycleHelpers: require('./lib/s3middleware/lifecycleHelpers'),
 };
 
 export const storage = {
     metadata: {
-        MetadataFileServer: require('./lib/storage/metadata/file/MetadataFileServer'),
-        MetadataFileClient: require('./lib/storage/metadata/file/MetadataFileClient'),
-        LogConsumer: require('./lib/storage/metadata/bucketclient/LogConsumer'),
+        MetadataWrapper: require('./lib/storage/metadata/MetadataWrapper'),
+        bucketclient: {
+            BucketClientInterface:
+            require('./lib/storage/metadata/bucketclient/' +
+                'BucketClientInterface'),
+            LogConsumer:
+            require('./lib/storage/metadata/bucketclient/LogConsumer'),
+        },
+        file: {
+            BucketFileInterface:
+            require('./lib/storage/metadata/file/BucketFileInterface'),
+            MetadataFileServer:
+            require('./lib/storage/metadata/file/MetadataFileServer'),
+            MetadataFileClient:
+            require('./lib/storage/metadata/file/MetadataFileClient'),
+        },
+        inMemory: {
+            metastore:
+            require('./lib/storage/metadata/in_memory/metastore'),
+            metadata: require('./lib/storage/metadata/in_memory/metadata'),
+            bucketUtilities:
+            require('./lib/storage/metadata/in_memory/bucket_utilities'),
+        },
+        mongoclient: {
+            MongoClientInterface:
+            require('./lib/storage/metadata/mongoclient/' +
+                'MongoClientInterface'),
+            LogConsumer:
+            require('./lib/storage/metadata/mongoclient/LogConsumer'),
+        },
+        proxy: {
+            Server: require('./lib/storage/metadata/proxy/Server'),
+        },
     },
     data: {
+        DataWrapper: require('./lib/storage/data/DataWrapper'),
+        MultipleBackendGateway:
+        require('./lib/storage/data/MultipleBackendGateway'),
+        parseLC: require('./lib/storage/data/LocationConstraintParser'),
         file: {
-            DataFileStore: require('./lib/storage/data/file/DataFileStore'),
+            DataFileStore:
+            require('./lib/storage/data/file/DataFileStore'),
+            DataFileInterface:
+            require('./lib/storage/data/file/DataFileInterface'),
+        },
+        external: {
+            AwsClient: require('./lib/storage/data/external/AwsClient'),
+            AzureClient: require('./lib/storage/data/external/AzureClient'),
+            GcpClient: require('./lib/storage/data/external/GcpClient'),
+            GCP: require('./lib/storage/data/external/GCP/GcpService'),
+            GcpUtils: require('./lib/storage/data/external/GCP/GcpUtils'),
+            GcpSigner: require('./lib/storage/data/external/GCP/GcpSigner'),
+            PfsClient: require('./lib/storage/data/external/PfsClient'),
+            backendUtils: require('./lib/storage/data/external/utils'),
+        },
+        inMemory: {
+            datastore: require('./lib/storage/data/in_memory/datastore'),
         },
     },
     utils: require('./lib/storage/utils'),
 };
 
 export const models = {
+    BackendInfo: require('./lib/models/BackendInfo'),
     BucketInfo: require('./lib/models/BucketInfo'),
+    BucketAzureInfo: require('./lib/models/BucketAzureInfo'),
     ObjectMD: require('./lib/models/ObjectMD'),
     ObjectMDLocation: require('./lib/models/ObjectMDLocation'),
+    ObjectMDAzureInfo: require('./lib/models/ObjectMDAzureInfo'),
     ARN: require('./lib/models/ARN'),
     WebsiteConfiguration: require('./lib/models/WebsiteConfiguration'),
-    ReplicationConfiguration: require('./lib/models/ReplicationConfiguration'),
-    LifecycleConfiguration: require('./lib/models/LifecycleConfiguration'),
+    ReplicationConfiguration:
+      require('./lib/models/ReplicationConfiguration'),
+    LifecycleConfiguration:
+        require('./lib/models/LifecycleConfiguration'),
     LifecycleRule: require('./lib/models/LifecycleRule'),
     BucketPolicy: require('./lib/models/BucketPolicy'),
-    ObjectLockConfiguration: require('./lib/models/ObjectLockConfiguration'),
-    NotificationConfiguration: require('./lib/models/NotificationConfiguration'),
+    ObjectLockConfiguration:
+        require('./lib/models/ObjectLockConfiguration'),
+    NotificationConfiguration:
+        require('./lib/models/NotificationConfiguration'),
 };
 
 export const metrics = {
@@ -139,4 +206,8 @@ export const pensieve = {
 
 export const stream = {
     readJSONStreamObject: require('./lib/stream/readJSONStreamObject'),
+};
+
+export const patches = {
+    locationConstraints: require('./lib/patches/locationConstraints'),
 };
