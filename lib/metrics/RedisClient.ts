6f588c00d71ed9e0f518cfb913f6c17ff387c392
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-04-14 14:40:33 +0200

    ARSN-147 Type RedisClient

--- lib/metrics/RedisClient.ts
+++ lib/metrics/RedisClient.ts
@@ -1,15 +1,13 @@
-const Redis = require('ioredis');
+import Redis from 'ioredis';
+import { Logger } from 'werelogs';
 
-class RedisClient {
-    /**
-    * @constructor
-    * @param {Object} config - config
-    * @param {string} config.host - Redis host
-    * @param {number} config.port - Redis port
-    * @param {string} config.password - Redis password
-    * @param {werelogs.Logger} logger - logger instance
-    */
-    constructor(config, logger) {
+export type Config = { host: string; port: number; password: string };
+export type Callback = (error: Error | null, value?: any) => void;
+
+export default class RedisClient {
+    _client: Redis.Redis;
+
+    constructor(config: Config, logger: Logger) {
         this._client = new Redis(config);
         this._client.on('error', err =>
             logger.trace('error from redis', {
@@ -22,65 +20,44 @@ class RedisClient {
         return this;
     }
 
-    /**
-    * increment value of a key by 1 and set a ttl
-    * @param {string} key - key holding the value
-    * @param {number} expiry - expiry in seconds
-    * @param {callback} cb - callback
-    * @return {undefined}
-    */
-    incrEx(key, expiry, cb) {
+    /** increment value of a key by 1 and set a ttl */
+    incrEx(key: string, expiry: number, cb: Callback) {
+        const exp = expiry.toString();
         return this._client
-            .multi([['incr', key], ['expire', key, expiry]])
+            .multi([['incr', key], ['expire', key, exp]])
             .exec(cb);
     }
 
-    /**
-     * increment value of a key by a given amount and set a ttl
-     * @param {string} key - key holding the value
-     * @param {number} amount - amount to increase by
-     * @param {number} expiry - expiry in seconds
-     * @param {callback} cb - callback
-     * @return {undefined}
-     */
-    incrbyEx(key, amount, expiry, cb) {
+    /** increment value of a key by a given amount and set a ttl */
+    incrbyEx(key: string, amount: number, expiry: number, cb: Callback) {
+        const am = amount.toString();
+        const exp = expiry.toString();
         return this._client
-            .multi([['incrby', key, amount], ['expire', key, expiry]])
+            .multi([['incrby', key, am], ['expire', key, exp]])
             .exec(cb);
     }
 
-    /**
-    * execute a batch of commands
-    * @param {string[]} cmds - list of commands
-    * @param {callback} cb - callback
-    * @return {undefined}
-    */
-    batch(cmds, cb) {
+    /** execute a batch of commands */
+    batch(cmds: string[][], cb: Callback) {
         return this._client.pipeline(cmds).exec(cb);
     }
 
     /**
      * Checks if a key exists
-     * @param {string} key - name of key
-     * @param {function} cb - callback
+     * @param cb - callback
      *   If cb response returns 0, key does not exist.
      *   If cb response returns 1, key exists.
-     * @return {undefined}
      */
-    exists(key, cb) {
+    exists(key: string, cb: Callback) {
         return this._client.exists(key, cb);
     }
 
     /**
      * Add a value and its score to a sorted set. If no sorted set exists, this
      * will create a new one for the given key.
-     * @param {string} key - name of key
-     * @param {integer} score - score used to order set
-     * @param {string} value - value to store
-     * @param {callback} cb - callback
-     * @return {undefined}
+     * @param score - score used to order set
      */
-    zadd(key, score, value, cb) {
+    zadd(key: string, score: number, value: string, cb: Callback) {
         return this._client.zadd(key, score, value, cb);
     }
 
@@ -89,11 +66,8 @@ class RedisClient {
      * Note: using this on a key that does not exist will return 0.
      * Note: using this on an existing key that isn't a sorted set will
      * return an error WRONGTYPE.
-     * @param {string} key - name of key
-     * @param {function} cb - callback
-     * @return {undefined}
      */
-    zcard(key, cb) {
+    zcard(key: string, cb: Callback) {
         return this._client.zcard(key, cb);
     }
 
@@ -102,55 +76,47 @@ class RedisClient {
      * Note: using this on a key that does not exist will return nil.
      * Note: using this on a value that does not exist in a valid sorted set key
      *       will return nil.
-     * @param {string} key - name of key
-     * @param {string} value - value within sorted set
-     * @param {function} cb - callback
-     * @return {undefined}
      */
-    zscore(key, value, cb) {
+     zscore(key: string, value: string, cb: Callback) {
         return this._client.zscore(key, value, cb);
     }
 
     /**
      * Remove a value from a sorted set
-     * @param {string} key - name of key
-     * @param {string|array} value - value within sorted set. Can specify
-     *   multiple values within an array
+     * @param value - value within sorted set. Can specify multiple values within an array
      * @param {function} cb - callback
      *   The cb response returns number of values removed
-     * @return {undefined}
      */
-    zrem(key, value, cb) {
+    zrem(key: string, value: string | string[], cb: Callback) {
         return this._client.zrem(key, value, cb);
     }
 
     /**
      * Get specified range of elements in a sorted set
-     * @param {string} key - name of key
-     * @param {integer} start - start index (inclusive)
-     * @param {integer} end - end index (inclusive) (can use -1)
-     * @param {function} cb - callback
-     * @return {undefined}
+     * @param start - start index (inclusive)
+     * @param end - end index (inclusive) (can use -1)
      */
-    zrange(key, start, end, cb) {
+    zrange(key: string, start: number, end: number, cb: Callback) {
         return this._client.zrange(key, start, end, cb);
     }
 
     /**
      * Get range of elements in a sorted set based off score
-     * @param {string} key - name of key
-     * @param {integer|string} min - min score value (inclusive)
+     * @param min - min score value (inclusive)
      *   (can use "-inf")
-     * @param {integer|string} max - max score value (inclusive)
+     * @param max - max score value (inclusive)
      *   (can use "+inf")
-     * @param {function} cb - callback
-     * @return {undefined}
      */
-    zrangebyscore(key, min, max, cb) {
+    zrangebyscore(
+        key: string,
+        min: number | string,
+        max: number | string,
+        cb: Callback,
+    ) {
         return this._client.zrangebyscore(key, min, max, cb);
     }
 
-    clear(cb) {
+    clear(cb: Callback) {
         return this._client.flushdb(cb);
     }
 
@@ -158,5 +124,3 @@ class RedisClient {
         this._client.disconnect();
     }
 }
-
-module.exports = RedisClient;
