8271b3ba216844c38d50244c07b41004e5e417e0
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-04-27 17:59:45 +0200

    ARSN-161 Type HealthProbeServer and Utils.ts

--- lib/network/index.ts
+++ lib/network/index.ts
@@ -3,6 +3,8 @@ import * as utils from './http/utils';
 import RESTServer from './rest/RESTServer';
 import RESTClient from './rest/RESTClient';
 import * as ProbeServer from './probe/ProbeServer';
+import HealthProbeServer from './probe/HealthProbeServer';
+import * as Utils from './probe/Utils';
 
 export const http = { server, utils };
 export const rest = { RESTServer, RESTClient };
