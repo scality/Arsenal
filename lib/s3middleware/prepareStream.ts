a0876d3df5e749024931a764f825b30d4f1a019c
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-04-28 19:32:50 +0200

    ARSN-171 Type prepareStream and refactor V4Transform to export type

--- lib/s3middleware/prepareStream.ts
+++ lib/s3middleware/prepareStream.ts
@@ -1,17 +1,25 @@
-const V4Transform = require('../auth/v4/streamingV4/V4Transform').default;
+import V4Transform, { TransformParams } from '../auth/v4/streamingV4/V4Transform';
+import Vault from '../auth/Vault';
+import * as werelogs from 'werelogs';
 
 /**
  * Prepares the stream if the chunks are sent in a v4 Auth request
- * @param {object} stream - stream containing the data
- * @param {object | null } streamingV4Params - if v4 auth, object containing
+ * @param stream - stream containing the data
+ * @param streamingV4Params - if v4 auth, object containing
  * accessKey, signatureFromRequest, region, scopeDate, timestamp, and
  * credentialScope (to be used for streaming v4 auth if applicable)
- * @param {object} vault - Vault instance passed from CloudServer
- * @param {RequestLogger} log - the current request logger
- * @param {function} cb - callback containing the result for V4Transform
- * @return {object} - V4Transform object if v4 Auth request, or else the stream
+ * @param vault - Vault instance passed from CloudServer
+ * @param log - the current request logger
+ * @param cb - callback containing the result for V4Transform
+ * @return - V4Transform object if v4 Auth request, or else the stream
  */
-function prepareStream(stream, streamingV4Params, vault, log, cb) {
+export function prepareStream(
+    stream: any,
+    streamingV4Params: TransformParams,
+    vault: Vault,
+    log: werelogs.Logger,
+    cb: any,
+) {
     if (stream.headers['x-amz-content-sha256'] ===
         'STREAMING-AWS4-HMAC-SHA256-PAYLOAD') {
         const v4Transform = new V4Transform(streamingV4Params, vault, log, cb);
@@ -20,7 +28,3 @@ function prepareStream(stream, streamingV4Params, vault, log, cb) {
     }
     return stream;
 }
-
-module.exports = {
-    prepareStream,
-};
