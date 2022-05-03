fd16eaaea50b0fab752622cb6303e9ccb81e36d7
williamlardier <william.lardier@scality.com>
2022-05-03 16:03:42 +0200

    ARSN-177: add tests for new restore field

---
+++ tests/unit/models/ObjectMDArchive.spec.js
@@ -0,0 +1,84 @@
+const assert = require('assert');
+const ObjectMDArchive = require('../../../lib/models/ObjectMDArchive');
+
+const testArchive = {
+    archiveInfo: {
+        any: 'value',
+    },
+    restoreRequestedAt: new Date(0),
+    restoreRequestedDays: 5,
+    restoreCompletedAt: new Date(1000),
+    restoreWillExpireAt: new Date(10000),
+};
+
+const archive = new ObjectMDArchive(
+    testArchive.archiveInfo,
+    testArchive.restoreRequestedAt,
+    testArchive.restoreRequestedDays,
+    testArchive.restoreCompletedAt,
+    testArchive.restoreWillExpireAt,
+);
+
+describe('ObjectMDArchive value', () => {
+    it('should return the correct value', () => {
+        const amzRestoreObj = archive.getValue();
+        assert.deepStrictEqual(amzRestoreObj, archive);
+    });
+});
+
+describe('ObjectMDArchive setters/getters', () => {
+    let archived = null;
+    beforeEach(() => {
+        archived = new ObjectMDArchive(
+            testArchive.archiveInfo,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+        );
+    });
+    it('should control the archiveInfo attribute', () => {
+        const info = {
+            test: 'data',
+        };
+        archive.setArchiveInfo(info);
+        assert.deepStrictEqual(archive.getArchiveInfo(),
+            info);
+    });
+    it('should control the restoreRequestedAt attribute', () => {
+        const requestedAt = new Date(123456);
+        assert.doesNotThrow(() => {
+            archived.setRestoreRequestedAt(requestedAt);
+        });
+        archive.setRestoreRequestedAt(requestedAt);
+        assert.deepStrictEqual(archive.getRestoreRequestedAt(),
+            requestedAt);
+    });
+    it('should control the restoreRequestedDays attribute', () => {
+        const requestedDays = 8;
+        assert.doesNotThrow(() => {
+            archived.setRestoreRequestedDays(requestedDays);
+        });
+        archive.setRestoreRequestedDays(requestedDays);
+        assert.deepStrictEqual(archive.getRestoreRequestedDays(),
+            requestedDays);
+    });
+    it('should control the restoreCompletedAt attribute', () => {
+        const completedAt = new Date(123456);
+        assert.throws(() => {
+            archived.setRestoreCompletedAt(completedAt);
+        });
+        archive.setRestoreCompletedAt(completedAt);
+        assert.deepStrictEqual(archive.getRestoreCompletedAt(),
+            completedAt);
+    });
+    it('should control the restoreWillExpireAt attribute', () => {
+        const willExpireAt = new Date(123456);
+        assert.throws(() => {
+            archived.setRestoreWillExpireAt(willExpireAt);
+        });
+        archive.setRestoreWillExpireAt(willExpireAt);
+        assert.deepStrictEqual(archive.getRestoreWillExpireAt(),
+            willExpireAt);
+    });
+});
