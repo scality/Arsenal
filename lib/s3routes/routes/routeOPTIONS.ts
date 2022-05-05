7cecbe27be7dedbaa10a1162bae570d5edcac1ca
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-05-05 17:50:35 +0200

    ARSN-184 Type check routeOPTIONS

--- lib/s3routes/routes/routeOPTIONS.ts
+++ lib/s3routes/routes/routeOPTIONS.ts
@@ -1,7 +1,15 @@
 import * as routesUtils from '../routesUtils';
 import errors from '../../errors';
+import * as http from 'http';
+import StatsClient from '../../metrics/StatsClient';
 
-export default function routeOPTIONS(request, response, api, log, statsClient) {
+export default function routeOPTIONS(
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+    api: { callApiMethod: routesUtils.CallApiMethod },
+    log: RequestLogger,
+    statsClient?: StatsClient,
+) {
     log.debug('routing request', { method: 'routeOPTION' });
 
     const corsMethod = request.headers['access-control-request-method'] || null;
@@ -10,14 +18,14 @@ export default function routeOPTIONS(request, response, api, log, statsClient) {
         const msg = 'Insufficient information. Origin request header needed.';
         const err = errors.BadRequest.customizeDescription(msg);
         log.debug('missing origin', { method: 'routeOPTIONS', error: err });
-        return routesUtils.responseXMLBody(err, undefined, response, log);
+        return routesUtils.responseXMLBody(err, null, response, log);
     }
-    if (['GET', 'PUT', 'HEAD', 'POST', 'DELETE'].indexOf(corsMethod) < 0) {
+    if (['GET', 'PUT', 'HEAD', 'POST', 'DELETE'].indexOf(corsMethod ?? '') < 0) {
         const msg = `Invalid Access-Control-Request-Method: ${corsMethod}`;
         const err = errors.BadRequest.customizeDescription(msg);
         log.debug('invalid Access-Control-Request-Method',
             { method: 'routeOPTIONS', error: err });
-        return routesUtils.responseXMLBody(err, undefined, response, log);
+        return routesUtils.responseXMLBody(err, null, response, log);
     }
 
     return api.callApiMethod('corsPreflight', request, response, log,
