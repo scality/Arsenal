9f1ea09ee6fd0021c0e440649ecdf4be4d22b14f
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-05-12 15:42:15 +0200

    ARSN-183 Switch index.ts

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ export * as constants from './lib/constants';
 export * as https from './lib/https';
 export * as metrics from './lib/metrics';
 export * as network from './lib/network';
+export * as stream from './lib/stream';
 
 export const db = require('./lib/db');
 export const shuffle = require('./lib/shuffle');
@@ -166,8 +167,4 @@ export const models = {
 
 export const pensieve = {
     credentialUtils: require('./lib/executables/pensieveCreds/utils'),
-};
-
-export const stream = {
-    readJSONStreamObject: require('./lib/stream/readJSONStreamObject').default,
 };
