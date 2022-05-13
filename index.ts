8ec4a11a4bc335225cc945dac599a376d196f1f4
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-05-13 13:57:21 +0200

    ARSN-207 Fix tests and export

--- index.ts
+++ index.ts
@@ -28,10 +28,10 @@ export * as https from './lib/https';
 export * as metrics from './lib/metrics';
 export * as network from './lib/network';
 export * as stream from './lib/stream';
+export { default as stringHash } from './lib/stringHash';
 
 export const db = require('./lib/db');
 export const shuffle = require('./lib/shuffle');
-export const stringHash = require('./lib/stringHash');
 export const jsutil = require('./lib/jsutil');
 
 export const algorithms = {
