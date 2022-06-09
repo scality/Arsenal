cd9456b5105e6b67c4b1c6e2aee6f4e44f098bf7
Naren <narendran.rajendran@scality.com>
2022-06-08 17:13:17 -0700

    bf: ARSN-220 export isMasterKey in versioning module

--- lib/versioning/index.ts
+++ lib/versioning/index.ts
@@ -1,5 +1,5 @@
 export { VersioningConstants } from './constants';
-export { Version } from './Version';
+export { Version, isMasterKey } from './Version';
 export * as VersionID from './VersionID';
 export { default as WriteGatheringManager } from './WriteGatheringManager';
 export { default as WriteCache } from './WriteCache';
