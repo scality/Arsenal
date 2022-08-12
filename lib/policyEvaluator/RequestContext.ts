4f2b1ca960888e162cc733ea0b55af3b823ba1dc
Jonathan Gramain <jonathan.gramain@scality.com>
2022-08-11 18:09:54 -0700

    bugfix: ARSN-262 fixes/tests in RequestContext
    - remove "postXml" field, as it was a left-over from prototyping
    
    - handle fields related to tag conditions: requestObjTags,
      existingObjTag, needTagEval, those were missing from constructor
      params
    
    - fix a typo in serialization: requersterInfo -> requesterInfo
    
    - new unit tests for RequestContext
      constructor/serialize/deserialize/getters

--- lib/policyEvaluator/RequestContext.ts
+++ lib/policyEvaluator/RequestContext.ts
@@ -166,7 +166,6 @@ export default class RequestContext {
     _policyArn: string;
     _action?: string;
     _needQuota: boolean;
-    _postXml?: string;
     _requestObjTags: string | null;
     _existingObjTag: string | null;
     _needTagEval: boolean;
@@ -190,7 +189,9 @@ export default class RequestContext {
         securityToken: string,
         policyArn: string,
         action?: string,
-        postXml?: string,
+        requestObjTags?: string,
+        existingObjTag?: string,
+        needTagEval?: false,
     ) {
         this._headers = headers;
         this._query = query;
@@ -220,10 +221,9 @@ export default class RequestContext {
         this._policyArn = policyArn;
         this._action = action;
         this._needQuota = _actionNeedQuotaCheck[apiMethod] === true;
-        this._postXml = postXml;
-        this._requestObjTags = null;
-        this._existingObjTag = null;
-        this._needTagEval = false;
+        this._requestObjTags = requestObjTags || null;
+        this._existingObjTag = existingObjTag || null;
+        this._needTagEval = needTagEval || false;
         return this;
     }
 
@@ -236,7 +236,7 @@ export default class RequestContext {
             apiMethod: this._apiMethod,
             headers: this._headers,
             query: this._query,
-            requersterInfo: this._requesterInfo,
+            requesterInfo: this._requesterInfo,
             requesterIp: this._requesterIp,
             sslEnabled: this._sslEnabled,
             awsService: this._awsService,
@@ -252,7 +252,6 @@ export default class RequestContext {
             securityToken: this._securityToken,
             policyArn: this._policyArn,
             action: this._action,
-            postXml: this._postXml,
             requestObjTags: this._requestObjTags,
             existingObjTag: this._existingObjTag,
             needTagEval: this._needTagEval,
@@ -276,12 +275,27 @@ export default class RequestContext {
         if (resource) {
             obj.specificResource = resource;
         }
-        return new RequestContext(obj.headers, obj.query, obj.generalResource,
-            obj.specificResource, obj.requesterIp, obj.sslEnabled,
-            obj.apiMethod, obj.awsService, obj.locationConstraint,
-            obj.requesterInfo, obj.signatureVersion,
-            obj.authType, obj.signatureAge, obj.securityToken, obj.policyArn,
-            obj.action, obj.postXml);
+        return new RequestContext(
+            obj.headers,
+            obj.query,
+            obj.generalResource,
+            obj.specificResource,
+            obj.requesterIp,
+            obj.sslEnabled,
+            obj.apiMethod,
+            obj.awsService,
+            obj.locationConstraint,
+            obj.requesterInfo,
+            obj.signatureVersion,
+            obj.authType,
+            obj.signatureAge,
+            obj.securityToken,
+            obj.policyArn,
+            obj.action,
+            obj.requestObjTags,
+            obj.existingObjTag,
+            obj.needTagEval,
+        );
     }
 
     /**
@@ -623,26 +637,6 @@ export default class RequestContext {
      */
     isQuotaCheckNeeded() {
         return this._needQuota;
-    }
-
-    /**
-     * Set request post
-     *
-     * @param postXml - request post
-     * @return itself
-     */
-    setPostXml(postXml: string) {
-        this._postXml = postXml;
-        return this;
-    }
-
-    /**
-     * Get request post
-     *
-     * @return request post
-     */
-    getPostXml() {
-        return this._postXml;
     }
 
     /**
