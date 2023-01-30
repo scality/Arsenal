164053d1e8380df559f3e6cc3a2459cac364c906
Jonathan Gramain <jonathan.gramain@scality.com>
2023-01-30 16:07:03 +0100

    Revert "bugfix: ARSN-293 DelimiterMaster: default to vFormat=v0"
    This reverts commit fbb62ef17cb1a64bc5fd1df57cbcbdc99f1eefde.

--- lib/algos/list/delimiterMaster.ts
+++ lib/algos/list/delimiterMaster.ts
@@ -60,7 +60,7 @@ export class DelimiterMaster extends Delimiter {
             },
         }[this.vFormat]);
 
-        if (this.vFormat === BucketVersioningKeyFormat.v0) {
+        if (vFormat === BucketVersioningKeyFormat.v0) {
             // override Delimiter's implementation of NotSkipping for
             // DelimiterMaster logic (skipping versions and special
             // handling of delete markers and PHDs)
