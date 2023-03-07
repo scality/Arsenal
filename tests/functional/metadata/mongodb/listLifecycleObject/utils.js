219e128a935abc182bdcf3e6bcfcb4a96234cf76
Nicolas Humbert <nicolas.humbert@scality.com>
2023-03-07 17:51:31 -0500

    ARSN-311 Add logic to list non-current versions for Lifecycle
    DelimiterNonCurrent used for listing non-current version.The Metadata call returns the versions (V prefix).The MD response is then processed to only return the non-current versions that became non-current before a defined date: beforeDate.
    
    (cherry picked from commit 5d018860ec4bc2b2a19ec5897a56f01d91f6a3c3)

--- tests/functional/metadata/mongodb/listLifecycleObject/utils.js
+++ tests/functional/metadata/mongodb/listLifecycleObject/utils.js
@@ -17,15 +17,24 @@ const assert = require('assert');
 */
 function putBulkObjectVersions(metadata, bucketName, objName, objVal, params, versionNb, timestamp, logger, cb) {
     let count = 0;
+    const versionIds = [];
     return async.whilst(
         () => count < versionNb,
         cbIterator => {
             count++;
             const lastModified = new Date(timestamp + count).toISOString();
             const finalObjectVal = Object.assign(objVal, { 'last-modified': lastModified });
-            return metadata.putObjectMD(bucketName, objName, finalObjectVal, params,
-                logger, cbIterator);
-        }, cb);
+            return metadata.putObjectMD(bucketName, objName, finalObjectVal, params, logger, (err, data) => {
+                versionIds.push(JSON.parse(data).versionId);
+                return cbIterator(err, versionIds);
+            });
+        }, (err, expectedVersionIds) => {
+            // The last version is removed since it represents the current version.
+            expectedVersionIds.pop();
+            // array is reversed to be alligned with the version order (latest to oldest).
+            expectedVersionIds.reverse();
+            return cb(err, expectedVersionIds);
+        });
 }
 
 function makeBucketMD(bucketName) {
@@ -63,6 +72,9 @@ function assertContents(contents, expected) {
         assert.strictEqual(c.key, expected[i].key);
         assert.strictEqual(c.value.LastModified, expected[i].LastModified);
         assert.strictEqual(c.value.staleDate, expected[i].staleDate);
+        if (expected[i].VersionId) {
+            assert.strictEqual(c.value.VersionId, expected[i].VersionId);
+        }
     });
 }
 
