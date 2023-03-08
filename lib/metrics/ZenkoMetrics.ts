c01d26b55e8553d1fc0e660323e07f946826dba9
Naren <narendran.rajendran@scality.com>
2023-03-07 17:21:13 -0800

    upgrade prom-client

--- lib/metrics/ZenkoMetrics.ts
+++ lib/metrics/ZenkoMetrics.ts
@@ -1,26 +1,26 @@
 import promClient from 'prom-client';
 
-const collectDefaultMetricsIntervalMs =
-    process.env.COLLECT_DEFAULT_METRICS_INTERVAL_MS !== undefined ?
-        Number.parseInt(process.env.COLLECT_DEFAULT_METRICS_INTERVAL_MS, 10) :
-        10000;
+// const collectDefaultMetricsIntervalMs =
+//     process.env.COLLECT_DEFAULT_METRICS_INTERVAL_MS !== undefined ?
+//         Number.parseInt(process.env.COLLECT_DEFAULT_METRICS_INTERVAL_MS, 10) :
+//         10000;
 
-promClient.collectDefaultMetrics({ timeout: collectDefaultMetricsIntervalMs });
+// promClient.collectDefaultMetrics({ timeout: collectDefaultMetricsIntervalMs });
 
 export default class ZenkoMetrics {
-    static createCounter(params: promClient.CounterConfiguration) {
+    static createCounter(params: any) {
         return new promClient.Counter(params);
     }
 
-    static createGauge(params: promClient.GaugeConfiguration) {
+    static createGauge(params: any) {
         return new promClient.Gauge(params);
     }
 
-    static createHistogram(params: promClient.HistogramConfiguration) {
+    static createHistogram(params: any) {
         return new promClient.Histogram(params);
     }
 
-    static createSummary(params: promClient.SummaryConfiguration) {
+    static createSummary(params: any) {
         return new promClient.Summary(params);
     }
 
