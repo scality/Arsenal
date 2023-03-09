662c891b88e9a270dc25c8f7963d338ea0e0ca54
Naren <narendran.rajendran@scality.com>
2023-03-09 07:58:49 -0800

    collectDefaultMetrics fn

--- lib/metrics/ZenkoMetrics.ts
+++ lib/metrics/ZenkoMetrics.ts
@@ -1,23 +1,19 @@
 import promClient from 'prom-client';
 
-// 'timeout' property is not needed/supported
-// https://github.com/siimon/prom-client/blob/199b7d19f8c8c34ee8653264e8dc0e57b420074f/CHANGELOG.md#1200---2020-02-20
-promClient.collectDefaultMetrics();
-
 export default class ZenkoMetrics {
-    static createCounter(params: any) {
+    static createCounter(params: promClient.CounterConfiguration<string>) {
         return new promClient.Counter(params);
     }
 
-    static createGauge(params: any) {
+    static createGauge(params: promClient.GaugeConfiguration<string>) {
         return new promClient.Gauge(params);
     }
 
-    static createHistogram(params: any) {
+    static createHistogram(params: promClient.HistogramConfiguration<string>) {
         return new promClient.Histogram(params);
     }
 
-    static createSummary(params: any) {
+    static createSummary(params: promClient.SummaryConfiguration<string>) {
         return new promClient.Summary(params);
     }
 
@@ -25,11 +21,15 @@ export default class ZenkoMetrics {
         return promClient.register.getSingleMetric(name);
     }
 
-    static asPrometheus() {
+    static async asPrometheus() {
         return promClient.register.metrics();
     }
 
     static asPrometheusContentType() {
         return promClient.register.contentType;
+    }
+
+    static collectDefaultMetrics() {
+        promClient.collectDefaultMetrics();
     }
 }
