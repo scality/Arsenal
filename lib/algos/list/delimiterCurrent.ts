d7f8a20e0cfe783e57d299511128e1970fc94747
Nicolas Humbert <nicolas.humbert@scality.com>
2023-07-13 17:34:24 -0400

    Adapt delimiterCurrent for S3C  Metadata

--- lib/algos/list/delimiterCurrent.ts
+++ lib/algos/list/delimiterCurrent.ts
@@ -1,4 +1,5 @@
 const { Delimiter } = require('./delimiter');
+const { FILTER_ACCEPT, FILTER_END } = require('./tools');
 
 type ResultObject = {
     Contents: {
@@ -8,6 +9,8 @@ type ResultObject = {
     IsTruncated: boolean;
     NextMarker ?: string;
 };
+
+const DELIMITER_TIMEOUT_MS = 10 * 1000; // 10s
 
 /**
  * Handle object listing with parameters. This extends the base class Delimiter
@@ -25,6 +28,9 @@ class DelimiterCurrent extends Delimiter {
         super(parameters, logger, vFormat);
 
         this.beforeDate = parameters.beforeDate;
+        // used for monitoring
+        this.start = null;
+        this.evaluatedKeys = 0;
     }
 
     genMDParamsV1() {
@@ -35,7 +41,50 @@ class DelimiterCurrent extends Delimiter {
                 lt: this.beforeDate,
             };
         }
+
+        this.start = Date.now();
+
         return params;
+    }
+
+    _parse(s) {
+        let p;
+        try {
+            p = JSON.parse(s);
+        } catch (e: any) {
+            this.logger.warn(
+                'Could not parse Object Metadata while listing',
+                { err: e.toString() });
+        }
+        return p;
+    }
+
+    filter(obj) {
+        if (this.start && Date.now() - this.start > DELIMITER_TIMEOUT_MS) {
+            this.IsTruncated = true;
+            this.logger.info('listing stopped after expected internal timeout',
+                {
+                    timeoutMs: DELIMITER_TIMEOUT_MS,
+                    evaluatedKeys: this.evaluatedKeys,
+                });
+            return FILTER_END;
+        }
+        ++this.evaluatedKeys;
+
+        const parsedValue = this._parse(obj.value);
+        // if parsing fails, skip the key.
+        if (parsedValue) {
+            const lastModified = parsedValue['last-modified'];
+            // We then check if the current version is older than the "beforeDate"
+            if (!this.beforeDate || (lastModified && lastModified < this.beforeDate)) {
+                return super.filter(obj);
+            }
+            // In the event of a timeout occurring before any content is added, make sure to update NextMarker.
+            const key = this.getObjectKey(obj);
+            this.NextMarker = key;
+        }
+
+        return FILTER_ACCEPT;
     }
 
     result(): ResultObject {
