2abe4667e81daf4401fa5c4f4eb020144289b768
Maha Benzekri <maha.benzekri@scality.com>
2025-01-02 11:59:37 +0100

    fixup on test

--- tests/unit/policyEvaluator/requestUtils.spec.js
+++ tests/unit/policyEvaluator/requestUtils.spec.js
@@ -90,7 +90,7 @@ describe('requestUtils.getClientIp', () => {
             },
         });
         const result = requestUtils.getClientIp(request, configWithoutProxy);
-        assert.strictEqual(result, testProxyIp);
+        assert.strictEqual(result, testClientIp1);
     });
 
     it('should return client Ip address from socket info if the request comes via proxies and ' +
