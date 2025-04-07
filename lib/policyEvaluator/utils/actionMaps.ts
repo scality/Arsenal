4320a13a8425ca68019fa2d266fd6539cf8a5070
williamlardier <william.lardier@scality.com>
2025-04-07 11:18:47 +0200

    Use SUR instead of SCUBA for the service name
    - Scuba name is not something we want to expose
      to our clients.
    - Use the SUR name.
    
    Issue: ARSN-484
    (cherry picked from commit 4dc694cb7529da86820b27c9ac991989e1570eef)

--- lib/policyEvaluator/utils/actionMaps.ts
+++ lib/policyEvaluator/utils/actionMaps.ts
@@ -202,12 +202,15 @@ const actionMapMetadata = {
 };
 
 const actionMapScuba = {
-    GetMetrics: 'scuba:GetMetrics',
-    GetMetricsBatch: 'scuba:GetMetricsBatch',
-    AdminStartIngest: 'scuba:AdminStartIngest',
-    AdminStopIngest: 'scuba:AdminStopIngest',
-    AdminReadRaftCseq: 'scuba:AdminReadRaftCseq',
-    AdminTriggerRepair: 'scuba:AdminTriggerRepair',
+    GetMetrics: 'sur:GetMetrics',
+    GetMetricsBatch: 'sur:GetMetricsBatch',
+    AdminStartIngest: 'sur:AdminStartIngest',
+    AdminStopIngest: 'sur:AdminStopIngest',
+    AdminReadRaftCseq: 'sur:AdminReadRaftCseq',
+    AdminTriggerRepair: 'sur:AdminTriggerRepair',
+    AdminStartDownsample: 'sur:AdminStartDownsample',
+    AdminStopDownsample: 'sur:AdminStopDownsample',
+    AdminTriggerDownsample: 'sur:AdminTriggerDownsample',
 };
 
 export {
