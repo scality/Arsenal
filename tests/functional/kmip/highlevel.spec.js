b2af501db46f48f3e3e432a0e3938890c764de49
Mickael Bourgois <mickael.bourgois@scality.com>
2025-05-27 16:30:50 +0200

    ARSN-498: Test kmip retry on internal thales err

--- tests/functional/kmip/highlevel.spec.js
+++ tests/functional/kmip/highlevel.spec.js
@@ -15,6 +15,8 @@ const KMIPClient = require('../../../lib/network/kmip/Client').default;
 const {
     logger,
 } = require('../../utils/kmip/ersatz');
+const util = require('util');
+const sinon = require('sinon');
 
 class LoopbackServerTransport extends TransportTemplate {
     constructor(options) {
@@ -105,6 +107,95 @@ describe('KMIP High Level Driver', () => {
             assert(err.is.InternalError);
             assert(err.description.includes('ECONNREFUSED'));
             done();
+        });
+    });
+
+    describe('thales session revoked (mock 24h idle socket)', () => {
+        let kmsKey;
+        let encrypted;
+
+        const reason = 'General Failure';
+        const message = '[NCERRUnauthorizedAccess]: Token has been revoked';
+        /** LoopbackServerTransport function that reply */
+        let stub;
+        let fakeErrorFct;
+
+        let tlsWriteSpy;
+        let tlsEndSpy;
+        let tlsConnectSpy;
+
+        // Instantiate a KMIPClient, create a master key and data key, prepare stubs and spys
+        beforeEach(async () => {
+            kmipClient = new KMIPClient(defaultOptions, TTLVCodec, LoopbackServerTransport);
+            // healthcheck to start the connection and the kmipHandshake
+            void await util.promisify(kmipClient.healthcheck.bind(kmipClient))(logger);
+
+            kmsKey = await util.promisify(kmipClient.createBucketKey.bind(kmipClient))('mybucket', logger);
+
+            encrypted = await util.promisify(kmipClient.cipherDataKey.bind(kmipClient))(
+                1, kmsKey, Buffer.from('abc'), logger);
+
+            stub = sinon.stub(kmipClient.kmip.transport.channel, '_transform');
+            // function that can replace the stub to generate 1 or more errors
+            fakeErrorFct = (_cipherFunc, _request, cb) => {
+                kmipClient.kmip.transport.channel.stubbedResult = true;
+                cb(null, kmipClient.kmip.transport.channel.errorResponse(reason, message));
+                kmipClient.kmip.transport.channel.stubbedResult = false;
+            };
+
+            tlsWriteSpy = sinon.spy(kmipClient.kmip.transport.channel, 'write');
+            tlsEndSpy = sinon.spy(kmipClient.kmip.transport.channel, 'end');
+            tlsConnectSpy = sinon.spy(kmipClient.kmip.transport.channel, 'connect');
+        });
+
+        /**
+         * Assert the same message was written twice
+         * @returns {undefined}
+         */
+        function assertTlsSpys() {
+            assert.strictEqual(tlsEndSpy.callCount, 1,
+                `tls connection should have been ended once instead of ${tlsEndSpy.callCount}`);
+            assert.strictEqual(tlsConnectSpy.callCount, 1,
+                `tls connect should have been called once instead of ${tlsConnectSpy.callCount}`);
+            assert.strictEqual(tlsWriteSpy.callCount, 2,
+                `tls write should have been called twice because of retry instead of ${tlsWriteSpy.callCount}`);
+            assert.ok(tlsWriteSpy.getCall(0).args[0].equals(tlsWriteSpy.getCall(1).args[0]),
+                'tls write should have twice the same argument (same bytes)');
+            // ensure the same buffer reference is reused during retry as there is no reason
+            // to use more memory for the exact same message
+            assert.strictEqual(tlsWriteSpy.getCall(0).args[0], tlsWriteSpy.getCall(1).args[0],
+                'tls write should have twice the same argument (same buffer reference)');
+            assert.strictEqual(stub.callCount, 2,
+                `stub for encrypt should have been called twice instead of ${stub.callCount}`);
+        }
+
+        it('should retry without kmipHandshake on thales session revoked', async () => {
+            // the next call should return a thales session revoked error only once
+            stub
+                .onFirstCall()
+                .callsFake(fakeErrorFct)
+                .callThrough();
+
+            const retriedEncryption = await util.promisify(kmipClient.cipherDataKey.bind(kmipClient))(
+                1, kmsKey, Buffer.from('abc'), logger);
+
+            assert.ok(encrypted.equals(retriedEncryption),
+                'second call to cipherDataKey with a retry should match 1st call');
+            assertTlsSpys();
+        });
+
+        it('should retry and fail the second time on thales session revoked', async () => {
+            // continuously return a thales session revoked error
+            stub.callsFake(fakeErrorFct);
+
+            await assert.rejects(util.promisify(kmipClient.cipherDataKey.bind(kmipClient))(
+                1, kmsKey, Buffer.from('abc'), logger), (err) => {
+                assert.strictEqual(err.AccessDenied, true);
+                assert.strictEqual(err.description,
+                    `KMS (KMIP) error for Encrypt on ${kmsKey}. ${reason}: ${message}`);
+                return true;
+            });
+            assertTlsSpys();
         });
     });
 });
