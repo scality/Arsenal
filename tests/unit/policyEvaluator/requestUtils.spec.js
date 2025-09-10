1e91739275c9b6423d38e9115feab3c2e4c640f0
williamlardier <william.lardier@scality.com>
2025-09-10 12:19:33 +0200

    Remove test following toLowerCase removal
    Issue: ARSN-522

--- tests/unit/policyEvaluator/requestUtils.spec.js
+++ tests/unit/policyEvaluator/requestUtils.spec.js
@@ -28,27 +28,6 @@ describe('requestUtils.getClientIp', () => {
         assert.strictEqual(result, testClientIp1);
     });
 
-    it('should return client Ip address in the proxy case when the header has uppercases', () => {
-        const request = new DummyRequest({
-            headers: {
-                'x-forwarded-for': [testClientIp1, testProxyIp].join(','),
-            },
-            url: '/',
-            parsedHost: 'localhost',
-            socket: {
-                remoteAddress: testProxyIp,
-            },
-        });
-        const result = requestUtils.getClientIp(request, {
-            requests: {
-                viaProxy: true,
-                trustedProxyCIDRs: ['192.168.100.0/22'],
-                extractClientIPFromHeader: 'X-Forwarded-For',
-            },
-        });
-        assert.strictEqual(result, testClientIp1);
-    });
-
     it('should return client Ip address from socket info if the request is not forwarded from proxies', () => {
         const request = new DummyRequest({
             headers: {},
@@ -167,26 +146,6 @@ describe('requestUtils.getHttpProtocolSecurity', () => {
         });
         const result = requestUtils.getHttpProtocolSecurity(request, configWithoutProxy);
         assert.strictEqual(result, false);
-    });
-
-    it('should handle configured headers with uppercases', () => {
-        const request = new DummyRequest({
-            headers: {
-                'x-forwarded-proto': 'https',
-            },
-            socket: {
-                remoteAddress: testProxyIp,
-            },
-        });
-        const result = requestUtils.getHttpProtocolSecurity(request, {
-            requests: {
-                viaProxy: true,
-                trustedProxyCIDRs: ['192.168.100.0/22'],
-                extractClientIPFromHeader: 'X-Forwarded-For',
-                extractProtocolFromHeader: 'X-Forwarded-Proto',
-            },
-        });
-        assert.strictEqual(result, true);
     });
 });
 
