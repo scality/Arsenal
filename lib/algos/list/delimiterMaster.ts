565f989b1fac05460c59f901c1f2d53553088b67
DarkIsDude <edouard.comtet@gmail.com>
2026-08-19 15:54:13 +0200

    🎨 prettier reformat files touched by ARSN-617
    Issue: ARSN-617

--- lib/algos/list/delimiterMaster.ts
+++ lib/algos/list/delimiterMaster.ts
@@ -22,21 +22,21 @@ export const enum DelimiterMasterFilterStateId {
     SkippingVersionsV0 = 101,
     WaitVersionAfterPHDV0 = 102,
     SkippingGapV0 = 103,
-};
+}
 
 interface DelimiterMasterFilterState_SkippingVersionsV0 extends FilterState {
-    id: DelimiterMasterFilterStateId.SkippingVersionsV0,
-    masterKey: string,
-};
+    id: DelimiterMasterFilterStateId.SkippingVersionsV0;
+    masterKey: string;
+}
 
 interface DelimiterMasterFilterState_WaitVersionAfterPHDV0 extends FilterState {
-    id: DelimiterMasterFilterStateId.WaitVersionAfterPHDV0,
-    masterKey: string,
-};
+    id: DelimiterMasterFilterStateId.WaitVersionAfterPHDV0;
+    masterKey: string;
+}
 
 interface DelimiterMasterFilterState_SkippingGapV0 extends FilterState {
-    id: DelimiterMasterFilterStateId.SkippingGapV0,
-};
+    id: DelimiterMasterFilterStateId.SkippingGapV0;
+}
 
 export const enum GapCachingState {
     NoGapCache = 0, // there is no gap cache
@@ -44,15 +44,14 @@ export const enum GapCachingState {
     GapLookupInProgress = 2, // asynchronous gap lookup in progress
     GapCached = 3, // an upcoming or already skippable gap is cached
     NoMoreGap = 4, // the cache doesn't have any more gaps inside the listed range
-};
+}
 
 type GapCachingInfo_NoGapCache = {
     state: GapCachingState.NoGapCache;
 };
 
 type GapCachingInfo_NoCachedGap = {
-    state: GapCachingState.UnknownGap
-        | GapCachingState.GapLookupInProgress
+    state: GapCachingState.UnknownGap | GapCachingState.GapLookupInProgress;
     gapCache: GapCacheInterface;
 };
 
@@ -66,18 +65,18 @@ type GapCachingInfo_NoMoreGap = {
     state: GapCachingState.NoMoreGap;
 };
 
-type GapCachingInfo = GapCachingInfo_NoGapCache
+type GapCachingInfo =
+    | GapCachingInfo_NoGapCache
     | GapCachingInfo_NoCachedGap
     | GapCachingInfo_GapCached
     | GapCachingInfo_NoMoreGap;
-
 
 export const enum GapBuildingState {
     Disabled = 0, // no gap cache or no gap building needed (e.g. in V1 versioning format)
     NotBuilding = 1, // not currently building a gap (i.e. not listing within a gap)
     Building = 2, // currently building a gap (i.e. listing within a gap)
     Expired = 3, // not allowed to build due to exposure delay timeout
-};
+}
 
 type GapBuildingInfo_NothingToBuild = {
     state: GapBuildingState.Disabled | GapBuildingState.Expired;
@@ -119,16 +118,13 @@ type GapBuildingInfo_Building = {
     gapWeight: number;
 };
 
-type GapBuildingInfo = GapBuildingInfo_NothingToBuild
-    | GapBuildingInfo_NotBuilding
-    | GapBuildingInfo_Building;
+type GapBuildingInfo = GapBuildingInfo_NothingToBuild | GapBuildingInfo_NotBuilding | GapBuildingInfo_Building;
 
 /**
  * Handle object listing with parameters. This extends the base class Delimiter
  * to return the raw master versions of existing objects.
  */
 export class DelimiterMaster extends Delimiter {
-
     _gapCaching: GapCachingInfo;
     _gapBuilding: GapBuildingInfo;
     _refreshedBuildingParams: GapBuildingParams | null;
@@ -155,31 +151,32 @@ export class DelimiterMaster extends Delimiter {
             // handling of delete markers and PHDs)
             this.setKeyHandler(
                 DelimiterFilterStateId.NotSkipping,
-                this.keyHandler_NotSkippingPrefixNorVersionsV0.bind(this));
+                this.keyHandler_NotSkippingPrefixNorVersionsV0.bind(this),
+            );
 
             // add extra state handlers specific to DelimiterMaster with v0 format
             this.setKeyHandler(
                 DelimiterMasterFilterStateId.SkippingVersionsV0,
-                this.keyHandler_SkippingVersionsV0.bind(this));
+                this.keyHandler_SkippingVersionsV0.bind(this),
+            );
 
             this.setKeyHandler(
                 DelimiterMasterFilterStateId.WaitVersionAfterPHDV0,
-                this.keyHandler_WaitVersionAfterPHDV0.bind(this));
+                this.keyHandler_WaitVersionAfterPHDV0.bind(this),
+            );
 
-            this.setKeyHandler(
-                DelimiterMasterFilterStateId.SkippingGapV0,
-                this.keyHandler_SkippingGapV0.bind(this));
+            this.setKeyHandler(DelimiterMasterFilterStateId.SkippingGapV0, this.keyHandler_SkippingGapV0.bind(this));
 
             if (this.marker) {
                 // distinct initial state to include some special logic
                 // before the first master key is found that does not have
                 // to be checked afterwards
-                this.state = <DelimiterMasterFilterState_SkippingVersionsV0> {
+                this.state = <DelimiterMasterFilterState_SkippingVersionsV0>{
                     id: DelimiterMasterFilterStateId.SkippingVersionsV0,
                     masterKey: this.marker,
                 };
             } else {
-                this.state = <DelimiterFilterState_NotSkipping> {
+                this.state = <DelimiterFilterState_NotSkipping>{
                     id: DelimiterFilterStateId.NotSkipping,
                 };
             }
@@ -189,7 +186,8 @@ export class DelimiterMaster extends Delimiter {
             this.keyHandler_NotSkipping_Delimiter = this.keyHandlers[DelimiterFilterStateId.NotSkipping];
             this.setKeyHandler(
                 DelimiterFilterStateId.NotSkipping,
-                this.keyHandler_NotSkippingPrefixNorVersionsV1.bind(this));
+                this.keyHandler_NotSkippingPrefixNorVersionsV1.bind(this),
+            );
         }
         // in v1, we can directly use Delimiter's implementation,
         // which is already set to the proper state
@@ -220,16 +218,16 @@ export class DelimiterMaster extends Delimiter {
     getGapBuildingValidityPeriodMs(): number | null {
         let gapBuilding;
         switch (this._gapBuilding.state) {
-        case GapBuildingState.Disabled:
-            return null;
-        case GapBuildingState.Expired:
-            return 0;
-        case GapBuildingState.NotBuilding:
-            gapBuilding = <GapBuildingInfo_NotBuilding> this._gapBuilding;
-            break;
-        case GapBuildingState.Building:
-            gapBuilding = <GapBuildingInfo_Building> this._gapBuilding;
-            break;
+            case GapBuildingState.Disabled:
+                return null;
+            case GapBuildingState.Expired:
+                return 0;
+            case GapBuildingState.NotBuilding:
+                gapBuilding = <GapBuildingInfo_NotBuilding>this._gapBuilding;
+                break;
+            case GapBuildingState.Building:
+                gapBuilding = <GapBuildingInfo_Building>this._gapBuilding;
+                break;
         }
         const { gapCache, params } = gapBuilding;
         const elapsedTime = Date.now() - params.initTimestamp;
@@ -260,11 +258,7 @@ export class DelimiterMaster extends Delimiter {
      *   otherwise). Defaults to `gapCacheProxy.maxGapWeight / 2`.
      * @return {undefined}
      */
-    refreshGapCache(
-        gapCacheProxy: GapCacheInterface,
-        minGapWeight?: number,
-        triggerSaveGapWeight?: number
-    ): void {
+    refreshGapCache(gapCacheProxy: GapCacheInterface, minGapWeight?: number, triggerSaveGapWeight?: number): void {
         if (this.vFormat !== BucketVersioningKeyFormat.v0) {
             return;
         }
@@ -276,8 +270,7 @@ export class DelimiterMaster extends Delimiter {
         }
         const refreshedBuildingParams: GapBuildingParams = {
             minGapWeight: minGapWeight || 100,
-            triggerSaveGapWeight: triggerSaveGapWeight
-                || Math.trunc(gapCacheProxy.maxGapWeight / 2),
+            triggerSaveGapWeight: triggerSaveGapWeight || Math.trunc(gapCacheProxy.maxGapWeight / 2),
             initTimestamp: Date.now(),
         };
         if (this._gapBuilding.state === GapBuildingState.Building) {
@@ -306,7 +299,7 @@ export class DelimiterMaster extends Delimiter {
         };
         const maxKey = this.prefix ? inc(this.prefix) : undefined;
         gapCaching.gapCache.lookupGap(fromKey, maxKey).then(_gap => {
-            const gap = <GapSetEntry | null> _gap;
+            const gap = <GapSetEntry | null>_gap;
             if (gap) {
                 this._gapCaching = {
                     state: GapCachingState.GapCached,
@@ -323,15 +316,15 @@ export class DelimiterMaster extends Delimiter {
 
     _checkGapOnMasterDeleteMarker(key: string): FilterReturnValue {
         switch (this._gapBuilding.state) {
-        case GapBuildingState.Disabled:
-        case GapBuildingState.Expired:
-            break;
-        case GapBuildingState.NotBuilding:
-            this._createBuildingGap(key, 1);
-            break;
-        case GapBuildingState.Building:
-            this._updateBuildingGap(key);
-            break;
+            case GapBuildingState.Disabled:
+            case GapBuildingState.Expired:
+                break;
+            case GapBuildingState.NotBuilding:
+                this._createBuildingGap(key, 1);
+                break;
+            case GapBuildingState.Building:
+                this._updateBuildingGap(key);
+                break;
         }
         if (this._gapCaching.state === GapCachingState.GapCached) {
             const { gapCached } = this._gapCaching;
@@ -339,7 +332,7 @@ export class DelimiterMaster extends Delimiter {
                 if (key <= gapCached.lastKey) {
                     // we are inside the last looked up cached gap: transition to
                     // 'SkippingGapV0' state
-                    this.setState(<DelimiterMasterFilterState_SkippingGapV0> {
+                    this.setState(<DelimiterMasterFilterState_SkippingGapV0>{
                         id: DelimiterMasterFilterStateId.SkippingGapV0,
                     });
                     // cut the current gap before skipping, it will be merged or
@@ -370,7 +363,7 @@ export class DelimiterMaster extends Delimiter {
         // adding the version to the contents
         if (Version.isDeleteMarker(value)) {
             // update the state to start skipping versions of the new master key
-            this.setState(<DelimiterMasterFilterState_SkippingVersionsV0> {
+            this.setState(<DelimiterMasterFilterState_SkippingVersionsV0>{
                 id: DelimiterMasterFilterStateId.SkippingVersionsV0,
                 masterKey: key,
             });
@@ -380,7 +373,7 @@ export class DelimiterMaster extends Delimiter {
             // master version is a PHD version: wait for the first
             // following version that will be considered as the actual
             // master key
-            this.setState(<DelimiterMasterFilterState_WaitVersionAfterPHDV0> {
+            this.setState(<DelimiterMasterFilterState_WaitVersionAfterPHDV0>{
                 id: DelimiterMasterFilterStateId.WaitVersionAfterPHDV0,
                 masterKey: key,
             });
@@ -391,7 +384,7 @@ export class DelimiterMaster extends Delimiter {
 
         if (key.startsWith(DbPrefixes.Replay)) {
             // skip internal replay prefix entirely
-            this.setState(<DelimiterFilterState_SkippingPrefix> {
+            this.setState(<DelimiterFilterState_SkippingPrefix>{
                 id: DelimiterFilterStateId.SkippingPrefix,
                 prefix: DbPrefixes.Replay,
             });
@@ -405,14 +398,14 @@ export class DelimiterMaster extends Delimiter {
         if (commonPrefix) {
             // transition into SkippingPrefix state to skip all following keys
             // while they start with the same prefix
-            this.setState(<DelimiterFilterState_SkippingPrefix> {
+            this.setState(<DelimiterFilterState_SkippingPrefix>{
                 id: DelimiterFilterStateId.SkippingPrefix,
                 prefix: commonPrefix,
             });
             return FILTER_ACCEPT;
         }
         // update the state to start skipping versions of the new master key
-        this.setState(<DelimiterMasterFilterState_SkippingVersionsV0> {
+        this.setState(<DelimiterMasterFilterState_SkippingVersionsV0>{
             id: DelimiterMasterFilterStateId.SkippingVersionsV0,
             masterKey: key,
         });
@@ -459,7 +452,7 @@ export class DelimiterMaster extends Delimiter {
         // 'masterKey == phdKey' is probably redundant when we already
         // know we have a versioned key, since all objects in v0 have
         // a master key, but keeping it in doubt)
-        const { masterKey: phdKey } = <DelimiterMasterFilterState_WaitVersionAfterPHDV0> this.state;
+        const { masterKey: phdKey } = <DelimiterMasterFilterState_WaitVersionAfterPHDV0>this.state;
         const versionIdIndex = key.indexOf(VID_SEP);
         if (versionIdIndex !== -1) {
             const masterKey = key.slice(0, versionIdIndex);
@@ -471,7 +464,7 @@ export class DelimiterMaster extends Delimiter {
     }
 
     keyHandler_SkippingGapV0(key: string, value: string): FilterReturnValue {
-        const { gapCache, gapCached } = <GapCachingInfo_GapCached> this._gapCaching;
+        const { gapCache, gapCached } = <GapCachingInfo_GapCached>this._gapCaching;
         if (key <= gapCached.lastKey) {
             return FILTER_SKIP;
         }
@@ -479,7 +472,7 @@ export class DelimiterMaster extends Delimiter {
             state: GapCachingState.UnknownGap,
             gapCache,
         };
-        this.setState(<DelimiterMasterFilterState_SkippingVersionsV0> {
+        this.setState(<DelimiterMasterFilterState_SkippingVersionsV0>{
             id: DelimiterMasterFilterStateId.SkippingVersionsV0,
         });
         // Start a gap with weight=0 from the latest skippable key. This will
@@ -492,18 +485,18 @@ export class DelimiterMaster extends Delimiter {
 
     skippingBase(): string | undefined {
         switch (this.state.id) {
-        case DelimiterMasterFilterStateId.SkippingVersionsV0: {
-            const { masterKey } = <DelimiterMasterFilterState_SkippingVersionsV0> this.state;
-            return masterKey + inc(VID_SEP);
-        }
+            case DelimiterMasterFilterStateId.SkippingVersionsV0: {
+                const { masterKey } = <DelimiterMasterFilterState_SkippingVersionsV0>this.state;
+                return masterKey + inc(VID_SEP);
+            }
 
-        case DelimiterMasterFilterStateId.SkippingGapV0: {
-            const { gapCached } = <GapCachingInfo_GapCached> this._gapCaching;
-            return gapCached.lastKey;
-        }
+            case DelimiterMasterFilterStateId.SkippingGapV0: {
+                const { gapCached } = <GapCachingInfo_GapCached>this._gapCaching;
+                return gapCached.lastKey;
+            }
 
-        default:
-            return super.skippingBase();
+            default:
+                return super.skippingBase();
         }
     }
 
@@ -529,8 +522,7 @@ export class DelimiterMaster extends Delimiter {
      * outside the allocated exposure time window.
      */
     _saveBuildingGap(): boolean {
-        const { gapCache, params, gap, gapWeight } =
-              <GapBuildingInfo_Building> this._gapBuilding;
+        const { gapCache, params, gap, gapWeight } = <GapBuildingInfo_Building>this._gapBuilding;
         const totalElapsed = Date.now() - params.initTimestamp;
         if (totalElapsed >= gapCache.exposureDelayMs) {
             this._gapBuilding = {
@@ -566,7 +558,7 @@ export class DelimiterMaster extends Delimiter {
      */
     _createBuildingGap(newKey: string, startWeight: number, cachedWeight?: number): void {
         if (this._gapBuilding.state === GapBuildingState.NotBuilding) {
-            const { gapCache, params } = <GapBuildingInfo_NotBuilding> this._gapBuilding;
+            const { gapCache, params } = <GapBuildingInfo_NotBuilding>this._gapBuilding;
             this._gapBuilding = {
                 state: GapBuildingState.Building,
                 gapCache,
@@ -582,7 +574,7 @@ export class DelimiterMaster extends Delimiter {
     }
 
     _updateBuildingGap(newKey: string): void {
-        const gapBuilding = <GapBuildingInfo_Building> this._gapBuilding;
+        const gapBuilding = <GapBuildingInfo_Building>this._gapBuilding;
         const { params, gap } = gapBuilding;
         gap.lastKey = newKey;
         gap.weight += 1;
@@ -591,15 +583,14 @@ export class DelimiterMaster extends Delimiter {
         // it once per update, by the known last key. In practice the default behavior
         // is to trigger an update after a number of keys that is half the maximum weight.
         // It is also useful for other listings to benefit from the cache sooner.
-        if (gapBuilding.gapWeight >= params.minGapWeight &&
-            gap.weight >= params.triggerSaveGapWeight) {
+        if (gapBuilding.gapWeight >= params.minGapWeight && gap.weight >= params.triggerSaveGapWeight) {
             this._saveBuildingGap();
         }
     }
 
     _cutBuildingGap(): void {
         if (this._gapBuilding.state === GapBuildingState.Building) {
-            let gapBuilding = <GapBuildingInfo_Building> this._gapBuilding;
+            let gapBuilding = <GapBuildingInfo_Building>this._gapBuilding;
             const { gapCache, gap, gapWeight } = gapBuilding;
             let params = gapBuilding.params;
             // only set gaps that are significant enough in weight and
@@ -610,7 +601,7 @@ export class DelimiterMaster extends Delimiter {
                     return;
                 }
                 // params may have been refreshed, reload them
-                gapBuilding = <GapBuildingInfo_Building> this._gapBuilding;
+                gapBuilding = <GapBuildingInfo_Building>this._gapBuilding;
                 params = gapBuilding.params;
             }
             this._gapBuilding = {
